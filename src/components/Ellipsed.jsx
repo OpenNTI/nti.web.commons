44d1e16e2f6713c4f3d38c653ecc42f648760cb3
Ray Hatfield <ray.hatfield@nextthought.com>
2016-05-20 17:07:13 -0500

    Moved Ellipsed component in from mobile; picked up nti-commons dependency.

---
+++ src/components/Ellipsed.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import setTextContent from 'react/lib/setTextContent';
+import SharedExecution from 'nti-commons/lib/SharedExecution';
+
+
+export default class Ellipsed extends React.Component {
+
+	static propTypes = {
+		tag: React.PropTypes.string,
+		measureOverflow: React.PropTypes.oneOf(['self', 'parent']).isRequired
+	}
+
+
+	static defaultProps = {
+		tag: 'div',
+		measureOverflow: 'self'
+	}
+
+
+	static trim = trim
+
+
+	componentDidMount () {
+		SharedExecution.clear(this.tt);
+		this.tt = truncateText(ReactDOM.findDOMNode(this), this.props.measureOverflow);
+	}
+
+
+	componentDidUpdate () {
+		SharedExecution.clear(this.tt);
+		this.tt = truncateText(ReactDOM.findDOMNode(this), this.props.measureOverflow);
+	}
+
+
+	render () {
+		let {tag} = this.props;
+		return React.createElement(tag, this.props);
+	}
+}
+
+function truncateText (el, measure) {
+	let textProperty = (el.textContent != null) ? 'textContent' : 'innerText';
+
+	let getText = () => el[textProperty];
+	let setText = text => setTextContent(el, text);
+
+
+	let setTitleOnce = () => {
+		el.setAttribute('title', getText());
+		setTitleOnce = () => {};
+	};
+
+	//if the element only has text nodes as children querySelector will return null.
+	if (el.querySelector('*')) {
+		console.error('EllipsisText is not safe on markup. Terminating.'); //eslint-disable-line
+		return;
+	}
+
+
+	return SharedExecution.schedual(() => {
+		const box = (measure === 'parent') ? el.parentNode : el;
+		const tooBig = () => box.scrollHeight - (box.clientHeight || box.offsetHeight) >= 1;
+
+		function trimStep () {
+			if (tooBig()) {
+				if (getText() !== '...') {
+					setTitleOnce();
+
+					setText(getText().replace(/[^\.](\.*)$/, '...'));
+
+					return SharedExecution.schedual(trimStep);
+				}
+			}
+		}
+
+		return trimStep();
+	});
+}
+
+
+export function trim (value, len, word) {
+	if (value && value.length > len) {
+		if (word) {
+			const vs = value.substr(0, len - 2);
+			const index = Math.max(
+					vs.lastIndexOf(' '),
+					vs.lastIndexOf('.'),
+					vs.lastIndexOf('!'),
+					vs.lastIndexOf('?')
+				);
+
+			if (index !== -1 && index >= (len - 15)) {
+				return vs.substr(0, index) + '...';
+			}
+		}
+		return value.substr(0, len - 3) + '...';
+	}
+
+	return value;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export AssetIcon from './AssetIcon';
 export DateTime from './DateTime';
+export Ellipsed from './Ellipsed';
