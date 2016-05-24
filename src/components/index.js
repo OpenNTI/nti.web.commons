c57b1bac305f699e12b93a4ce757a836ada6b422
Ray Hatfield <ray.hatfield@nextthought.com>
2016-05-24 15:37:01 -0500

    Pulling in more components from mobile

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,8 @@ export Ellipsed from './Ellipsed';
 export Error from './Error';
 export Loading from './Loading';
 export LoadingInline from './LoadingInline';
+export Notice from './Notice';
+export PanelButton from './PanelButton';
 export ScrollTrigger from './ScrollTrigger';
 export SelectBox from './SelectBox';
 export TinyLoader from './TinyLoader';
