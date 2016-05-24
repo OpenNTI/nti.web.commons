c354be4dc68349b3a6e721aa7e8c86c66c1acdc4
Ray Hatfield <ray.hatfield@nextthought.com>
2016-05-24 13:35:24 -0500

    pull Error component in from mobile

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export AssetIcon from './AssetIcon';
 export CalendarCard from './CalendarCard';
 export DateTime from './DateTime';
 export Ellipsed from './Ellipsed';
+export Error from './Error';
 export Loading from './Loading';
 export LoadingInline from './LoadingInline';
 export SelectBox from './SelectBox';
