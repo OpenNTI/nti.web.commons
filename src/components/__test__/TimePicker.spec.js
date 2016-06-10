ced771904fbb1950c0050bdf7c48abe1966f7d56
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-10 14:44:05 -0500

    add more tests to implement

--- src/components/__test__/TimePicker.spec.js
+++ src/components/__test__/TimePicker.spec.js
@@ -9,3 +9,8 @@ import TimePicker from '../TimePicker';
 // check if typed twentyFourHourTime works
 // check if wrapping twentyFourHourTime works
 // check errors
+
+//Test that passing a value prop and no onChnge handler, the value never updates no matter what the user does. (readonly mode)
+//Test that passing a value prop and onChnge handler, the value only updates what the parent passes as the value prop. (normal expected interaction)
+//Test that passing no value prop, and onChange the ui updates as user interacts. And onChange calls with new values. (lazy mode)
+//Test that no props behaves normally too, but should warn that there isn't a legitimate way of getting values/output.
