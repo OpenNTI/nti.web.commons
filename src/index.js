d76c0a7aa7362852127aeca721be1b4f56b1b438
Ray Hatfield <ray.hatfield@nextthought.com>
2016-06-17 11:34:17 -0500

    move prompts into commons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export * from './components';
+export * from './prompts';
 import * as Constants from './constants';
 import * as HOC from './HighOrderComponents';
 import * as Mixins from './mixins';
