eacccc14f5ab2a29240e0273d24f8299daa2e1da
Ray Hatfield <ray.hatfield@nextthought.com>
2016-06-20 18:28:21 -0500

    added AssetIcon test

---
+++ src/components/__test__/AssetIcon.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import AssetIcon from '../AssetIcon';
+
+describe('AssetIcon', () => {
+	it('should render a \'jpeg\' label', () => {
+		const wrapper = shallow(<AssetIcon mimeType="image/jpeg" />);
+		expect(wrapper.find('.file-type label').length === 1);
+		expect(wrapper.find('label').text()).toEqual('jpeg');
+	});
+});
