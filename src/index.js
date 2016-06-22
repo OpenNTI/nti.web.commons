ca061ef028026fa379070b92691a7eb4523b373b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-22 16:29:47 -0500

    use 'export * as'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 export * from './components';
 export * as Prompt from './prompts';
-import * as Constants from './constants';
-import * as HOC from './HighOrderComponents';
-import * as Mixins from './mixins';
-
-export {
-	Constants,
-	HOC,
-	Mixins
-};
+export * as Constants from './constants';
+export * as HOC from './HighOrderComponents';
+export * as Mixins from './mixins';
