a60972c50b81adc6185258663492d2fdb997d71d
pacimahoro <pacimahoro@gmail.com>
2016-06-24 12:42:59 -0500

    More styling and wiring for the available picker.

--- src/components/AvailablePicker.jsx
+++ src/components/AvailablePicker.jsx
@@ -1,20 +1,25 @@
 import React, { PropTypes } from 'react';
 import cx from 'classnames';
+import {scoped} from 'nti-lib-locale';
+import moment from 'moment';
 import autobind from 'nti-commons/lib/autobind';
 import DayTimePicker from './DayTimePicker';
 import Flyout from './Flyout';
 import DateTime from './DateTime';
+import Checkbox from './Checkbox';
 
 export default class AvailablePicker extends React.Component {
 	static propTypes = {
 		value: React.PropTypes.instanceOf(Date),
 		label: React.PropTypes.string,
+		checked: React.PropTypes.bool,
 		onChange: PropTypes.func
 	}
 
 	static defaultProps = {
 		value: new Date(),
 		label: 'Available Date',
+		checked: true,
 		onChange: () => {
 			console.warn('Pass on change prop');
 		},
@@ -29,17 +34,18 @@ export default class AvailablePicker extends React.Component {
 
 		this.setFlyoutRef = x => this.flyoutRef = x;
 
-		autobind(this, 'onDateChange', 'onSave', 'closeMenu');
+		autobind(this, 'onDateChange', 'onSave', 'onCheckChange');
 	}
 
 
 	setupValue (props = this.props) {
 		const setState = s => this.state ? this.setState(s) : (this.state = s);
-		const {value} = props;
+		const {value, checked} = props;
 		const date = (value instanceof Date) ? value : new Date();
 
 		setState({
-			date
+			date,
+			checked
 		});
 	}
 
@@ -49,6 +55,17 @@ export default class AvailablePicker extends React.Component {
 			changed: true,
 			dayClicked: true
 		});
+	}
+
+	onCheckChange (e) {
+		const newChecked = e.target.checked;
+		const {checked:oldChecked} = this.state;
+
+		if (newChecked !== oldChecked) {
+			this.setState({
+				checked: newChecked
+			});
+		}
 	}
 
 	onSave () {
@@ -63,14 +80,18 @@ export default class AvailablePicker extends React.Component {
 		}
 	}
 
+	getValue () {
+		const {date, checked} = this.state;
+		return checked ? date : null;
+	}
+
 	renderTrigger () {
 		const {label} = this.props;
 		const {date} = this.state;
-		const classNames = cx('available-trigger');
 		const format = 'L';
 
 		return (
-			<div className={classNames}>
+			<div className="available-trigger" onClick={this.onTriggerToggle}>
 				<div className="inner-available-trigger">
 					<div className="available-label">
 						{label}
@@ -85,16 +106,13 @@ export default class AvailablePicker extends React.Component {
 	}
 
 	render () {
-		const {date, changed} = this.state;
+		const {date, changed, checked} = this.state;
 		const {label} = this.props;
-		const saveClassNames = cx('available-save flyout-fullwidth-btn', {'changed': changed});
+		const saveClassNames = cx('available-save flyout-fullwidth-btn', {changed});
 		return (
 			<Flyout ref={this.setFlyoutRef} className="available-picker" alignment="bottom-left" trigger={this.renderTrigger()}>
-				{label}
-				<DayTimePicker
-					value={date}
-					onChange={this.onDateChange}
-				/>
+				<Checkbox label={label} checked={checked} onChange={this.onCheckChange} />
+				<DayTimePicker value={date} onChange={this.onDateChange} />
 				<div className={saveClassNames} onClick={changed === true ? this.onSave : null}>Save</div>
 			</Flyout>
 		);
