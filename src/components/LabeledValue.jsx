a05a2aec5fb290f8fab0baa7453462817817869f
Ray Hatfield <ray.hatfield@nextthought.com>
2016-06-27 17:12:49 -0500

    LabeledValue grows an arrow option

--- src/components/LabeledValue.jsx
+++ src/components/LabeledValue.jsx
@@ -4,11 +4,14 @@ import cx from 'classnames';
 LabeledValue.propTypes = {
 	label: React.PropTypes.any,
 	children: React.PropTypes.any,
-	className: React.PropTypes.string
+	className: React.PropTypes.string,
+	arrow: React.PropTypes.bool
 };
 
 export default function LabeledValue (props) {
-	const classes = cx('labeled-value', props.className);
+	const classes = cx('labeled-value', props.className, {
+		'arrow-down': props.arrow
+	});
 	return (
 		<div {...props} className={classes}>
 			<label>{props.label}</label>
