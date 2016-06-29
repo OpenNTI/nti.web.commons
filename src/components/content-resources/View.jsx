7e899772d54c701c2fe166fb5b1d014f1a9413a6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-29 12:36:07 -0500

    switch view to be a full component (still stateless)

--- src/components/content-resources/View.jsx
+++ src/components/content-resources/View.jsx
@@ -3,27 +3,34 @@ import cx from 'classnames';
 
 import IconGrid from './layout/icon-grid';
 
-ContentResourcesView.propTypes = {
-	children: PropTypes.any,
-	className: PropTypes.string,
-	layout: PropTypes.func,//React Component "Type"
-	...IconGrid.propTypes
-};
+export default class ContentResourcesView extends React.Component {
 
-export default function ContentResourcesView (props) {
-	const {children, className, contents, selection, layout} = props;
-	const hasSubView = !!children;
+	static propTypes = {
+		children: PropTypes.any,
+		className: PropTypes.string,
+		layout: PropTypes.func,//React Component "Type"
+		...IconGrid.propTypes
+	}
 
-	const Layout = layout || IconGrid;
 
-	return (
-		<div className={cx('content-resource-view', className, {split: hasSubView})}>
-			<div className="view-main-pane" onClick={()=> selection.set()}>
-				<Layout contents={contents} selection={selection}/>
+	clearSelection = () => this.props.selection.set()
+
+
+	render () {
+		const {children, className, contents, selection, layout} = this.props;
+		const hasSubView = !!children;
+
+		const Layout = layout || IconGrid;
+
+		return (
+			<div className={cx('content-resource-view', className, {split: hasSubView})}>
+				<div className="view-main-pane" onClick={this.clearSelection}>
+					<Layout contents={contents} selection={selection}/>
+				</div>
+				{hasSubView && (
+					<div className="context-view-pane">{children}</div>
+				)}
 			</div>
-			{hasSubView && (
-				<div className="context-view-pane">{children}</div>
-			)}
-		</div>
-	);
+		);
+	}
 }
