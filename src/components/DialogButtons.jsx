f9cec5df3cf1ce7f2c2ae55d9d846cd78bc3110a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-30 14:41:10 -0500

    lint

--- src/components/DialogButtons.jsx
+++ src/components/DialogButtons.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
 import cx from 'classnames';
 
-export default class DialogButtons extends React.Component {
+DialogButtons.propTypes = {
+	flat: React.PropTypes.bool,
+	buttons: React.PropTypes.array
+};
+export default function DialogButtons (props) {
+	const {buttons = [], flat} = props;
 
-	static propTypes = {
-		flat: React.PropTypes.bool,
-		buttons: React.PropTypes.array
-	}
+	const c = cx('dialog-buttons', {flat});
 
-	render () {
-
-		const {buttons = [], flat} = this.props;
-
-		const c = cx('dialog-buttons', {flat});
-
-		return (
-			<div className={c}>
-				{buttons.map( (b, i) => {
-					const classes = cx('button', b.className, {
-						primary: i === buttons.length - 1,
-						secondary: i < buttons.length - 1
-					});
-					return (
-						<div key={b.label}
-							className={classes}
-							onClick={b.onClick}>
-								{b.label}
-						</div>
-					);
-				})}
-			</div>
-		);
-	}
+	return (
+		<div className={c}>
+			{buttons.map( (b, i) => {
+				const classes = cx('button', b.className, {
+					primary: i === buttons.length - 1,
+					secondary: i < buttons.length - 1
+				});
+				return (
+					<div key={b.label}
+						className={classes}
+						onClick={b.onClick}>
+							{b.label}
+					</div>
+				);
+			})}
+		</div>
+	);
 }
