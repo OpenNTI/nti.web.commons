f49e086c661464d438bc6fe20a3f61827e91c113
Josh Birdwell <jbirdwell34@gmail.com>
2016-06-30 23:29:14 -0500

    Update checkbox, radio box assets. Start work for color prop

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -4,14 +4,15 @@ Checkbox.propTypes = {
 	label: PropTypes.string,
 	checked: PropTypes.bool,
 	children: PropTypes.any,
-	name: PropTypes.string
+	name: PropTypes.string,
+	color: PropTypes.string
 };
 
 export default function Checkbox (props) {
 	return (
 		<label className="checkbox-component" name={props.name}>
 			<input {...props} type="checkbox" children={void 0}/>
-			<span className="label">{props.label}</span>
+			<span className={`label ${props.color}`}>{props.label}</span>
 			{props.children && props.checked && (
 				<div className="sub">
 					{props.children}
