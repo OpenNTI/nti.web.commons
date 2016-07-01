f2e50f76b3b10f8658d55a85e33c98b941b3f8c3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-30 22:29:37 -0500

    support disabled

--- src/components/FilePickerButton.jsx
+++ src/components/FilePickerButton.jsx
@@ -4,14 +4,15 @@ import cx from 'classnames';
 ToolbarButton.propTypes = {
 	className: PropTypes.string,
 	children: PropTypes.any,
+	disabled: PropTypes.bool,
 	icon: PropTypes.string,
 	label: PropTypes.string
 };
 
 export default function ToolbarButton (props) {
-	const {children, className, label, icon} = props;
+	const {children, className, disabled, label, icon} = props;
 	return (
-		<span {...props} className={cx('button', 'file-picker', className)} role="button" data-tip={label} aria-label={label}>
+		<span {...props} className={cx('button', 'file-picker', className, {disabled})} role="button" data-tip={label} aria-label={label}>
 			<input type="file" {...props}/>
 			<span>
 				<i className={`small icon-${icon}`}/>
