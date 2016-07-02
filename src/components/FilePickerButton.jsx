aec6ad14aa21e47c4bac073d8f94cd632f223a58
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-01 22:43:56 -0500

    fix name

--- src/components/FilePickerButton.jsx
+++ src/components/FilePickerButton.jsx
@@ -1,7 +1,7 @@
 import React, {PropTypes} from 'react';
 import cx from 'classnames';
 
-ToolbarButton.propTypes = {
+FilePickerButton.propTypes = {
 	className: PropTypes.string,
 	children: PropTypes.any,
 	disabled: PropTypes.bool,
@@ -9,7 +9,7 @@ ToolbarButton.propTypes = {
 	label: PropTypes.string
 };
 
-export default function ToolbarButton (props) {
+export default function FilePickerButton (props) {
 	const {children, className, disabled, label, icon} = props;
 	return (
 		<span {...props} className={cx('button', 'file-picker', className, {disabled})} role="button" data-tip={label} aria-label={label}>
