04a6e2657cf05d15b613f184e73787a905a446a7
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-01 22:44:31 -0500

    add available support

--- src/components/FilePickerButton.jsx
+++ src/components/FilePickerButton.jsx
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react';
 import cx from 'classnames';
 
 FilePickerButton.propTypes = {
+	available: PropTypes.bool,
 	className: PropTypes.string,
 	children: PropTypes.any,
 	disabled: PropTypes.bool,
@@ -10,7 +11,12 @@ FilePickerButton.propTypes = {
 };
 
 export default function FilePickerButton (props) {
-	const {children, className, disabled, label, icon} = props;
+	const {available, children, className, disabled, label, icon} = props;
+
+	if ('available' in props && !available) {
+		return null;
+	}
+
 	return (
 		<span {...props} className={cx('button', 'file-picker', className, {disabled})} role="button" data-tip={label} aria-label={label}>
 			<input type="file" {...props}/>
