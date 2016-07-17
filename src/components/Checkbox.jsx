8bb0d089c491562adc6c26b4d1f19f32e664e4f3
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-17 15:35:29 -0500

    Update Checkbox & Radio to support 'green' skins

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,21 +1,23 @@
 import React, {PropTypes} from 'react';
+import cx from 'classnames';
 
 Checkbox.propTypes = {
-	label: PropTypes.string,
 	checked: PropTypes.bool,
 	children: PropTypes.any,
-	name: PropTypes.string,
-	color: PropTypes.string
+	green: PropTypes.bool,
+	label: PropTypes.string,
+	name: PropTypes.string
 };
 
 export default function Checkbox (props) {
+	const {checked, children, green, label, name, ...otherProps} = props;
 	return (
-		<label className="checkbox-component" name={props.name}>
-			<input {...props} type="checkbox" children={void 0}/>
-			<span className={`label ${props.color}`}>{props.label}</span>
-			{props.children && props.checked && (
+		<label className="checkbox-component" name={name}>
+			<input {...otherProps} type="checkbox" children={void 0}/>
+			<span className={cx('label', {green})}>{label}</span>
+			{children && checked && (
 				<div className="sub">
-					{props.children}
+					{children}
 				</div>
 			)}
 		</label>
--- src/components/Radio.jsx
+++ src/components/Radio.jsx
@@ -1,20 +1,24 @@
 import React, {PropTypes} from 'react';
+import cx from 'classnames';
+
 
 Radio.propTypes = {
-	label: PropTypes.string,
 	checked: PropTypes.bool,
 	children: PropTypes.any,
+	green: PropTypes.bool,
+	label: PropTypes.string,
 	name: PropTypes.string
 };
 
 export default function Radio (props) {
+	const {checked, children, green, label, name, ...otherProps} = props;
 	return (
-		<label className="radio-component" name={props.name}>
-			<input {...props} type="radio" children={void 0}/>
-			<span className="label">{props.label}</span>
-			{props.children && props.checked && (
+		<label className="radio-component" name={name}>
+			<input {...otherProps} type="radio" children={void 0}/>
+			<span className={cx('label', {green})}>{label}</span>
+			{children && checked && (
 				<div className="sub">
-					{props.children}
+					{children}
 				</div>
 			)}
 		</label>
