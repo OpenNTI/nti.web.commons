f3b344d2da1a25082103963fd2958704b2628f61
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-26 12:18:00 -0500

    Wireup tags & comment

--- src/components/content-resources/inspection/Comment.jsx
+++ src/components/content-resources/inspection/Comment.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import {scoped} from 'nti-lib-locale';
+import buffered from 'nti-commons/lib/function-buffer';
+
+import ItemChanges from '../../../HighOrderComponents/ItemChanges';
 
 import LabeledValue from '../../LabeledValue';
 
@@ -11,21 +14,48 @@ const DEFAULT_TEXT = {
 
 const t = scoped('common.components.content-resources.inspector', DEFAULT_TEXT);
 
-export default class Comments extends React.Component {
+const BUFFERED_TIME = 2000;//two seconds
+
+class Comment extends React.Component {
 	static propTypes = {
 		item: React.PropTypes.object
 	}
 
-	state = {}
+	attachRef = x => this.commentField = x
+
+
+	onChange = buffered(BUFFERED_TIME, () => {
+		//TODO: Waiting on a comment field from the server.
+		// const {item} = this.props;
+		// const {value: comment} = this.commentField;
+		//
+		// item.save({comment});
+	})
+
+
+	componentWillUnmount () {
+		if (this.onChange.pending) {
+			this.onChange.flush();
+		}
+	}
+
 
 	render () {
 		const {item} = this.props;
+		const disabled = !item.hasLink('edit') || !item.hasOwnProperty('comment');
 		return (
 			<div className="resource-viewer-inspector-file-tags">
 				<LabeledValue label={t('Comment')}>
-					<textarea value={item.comment} placeholder={t('CommentPlaceholder')}/>
+					<textarea value={item.comment}
+						disabled={disabled}
+						placeholder={disabled ? void 0 : t('CommentPlaceholder')}
+						onChange={this.onChange}
+						ref={this.attachRef}
+						/>
 				</LabeledValue>
 			</div>
 		);
 	}
 }
+
+export default ItemChanges.compose(Comment);
--- src/components/content-resources/inspection/Tags.jsx
+++ src/components/content-resources/inspection/Tags.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import {scoped} from 'nti-lib-locale';
+import buffered from 'nti-commons/lib/function-buffer';
+
+import ItemChanges from '../../../HighOrderComponents/ItemChanges';
 
 import LabeledValue from '../../LabeledValue';
 import TagField from '../../TokenEditor';
@@ -12,23 +15,47 @@ const DEFAULT_TEXT = {
 
 const t = scoped('common.components.content-resources.inspector', DEFAULT_TEXT);
 
-export default class Tags extends React.Component {
+const BUFFERED_TIME = 2000;//two seconds
+
+class Tags extends React.Component {
 	static propTypes = {
 		item: React.PropTypes.object
 	}
 
+	attachRef = x => this.tagField = x
 
-	onNewTags = () => console.log(arguments)
+
+	onChange = buffered(BUFFERED_TIME, () => {
+		const {item} = this.props;
+		const {value: tags} = this.tagField;
+
+		item.save({tags});
+	})
+
+
+	componentWillUnmount () {
+		if (this.onChange.pending) {
+			this.onChange.flush();
+		}
+	}
 
 
 	render () {
 		const {item} = this.props;
+
 		return (
 			<div className="resource-viewer-inspector-file-tags">
 				<LabeledValue label={t('Tags')}>
-					<TagField value={item.tags} placeholder={t('TagsPlaceholder')} onChange={this.onNewTags}/>
+					<TagField value={item.tags}
+						disabled={!item.hasLink('edit')}
+						placeholder={t('TagsPlaceholder')}
+						onChange={this.onChange}
+						ref={this.attachRef}
+						/>
 				</LabeledValue>
 			</div>
 		);
 	}
 }
+
+export default ItemChanges.compose(Tags);
