78b8650703665342c89d848afcf90f787d04ac6f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-07-28 17:18:35 -0500

    Allow search to be unbuffered

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,16 +4,49 @@ import BufferedInput from './BufferedInput';
 
 const stop = e => e.preventDefault();
 
+
+class Input extends React.Component {
+
+	attachRef = x => this.input = x
+
+	clear () {
+		const {input, props: {onChange}} = this; //eslint-disable-line
+		input.value = '';
+		if (onChange) {
+			onChange({
+				target: input,
+				type: 'change',
+				persist () {},
+				preventDefault () {},
+				stopPropagation () {}
+			});
+		}
+	}
+
+	render () {
+		return ( <input {...this.props} ref={this.attachRef}/> );
+	}
+}
+
+
+
 export default class Search extends React.Component {
 
 	static propTypes = {
 		disabled: PropTypes.bool,
 		onChange: PropTypes.func,
-		defaultValue: PropTypes.string
+		defaultValue: PropTypes.string,
+		buffered: PropTypes.bool
+	}
+
+
+	static defaultProps = {
+		buffered: true
 	}
 
 
 	attachRef = x => this.input = x
+
 
 	clearFilter = () => {
 		const {input} = this;
@@ -29,15 +62,17 @@ export default class Search extends React.Component {
 		}
 	}
 
+
 	render () {
-		const {disabled, defaultValue} = this.props;
+		const {disabled, buffered, ...props} = this.props;
+		const Cmp = buffered ? BufferedInput : Input;
 		return (
 			<form onSubmit={stop} className={cx('search-component',{disabled})}>
 				<i className="icon-search"/>
-				<BufferedInput type="text"
+				<Cmp {...props}
+					type="text"
 					placeholder="Search"
 					disabled={disabled}
-					defaultValue={defaultValue}
 					onChange={this.onChange}
 					ref={this.attachRef}
 					required
