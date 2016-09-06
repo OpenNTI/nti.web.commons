a30ed35847222efbb0947e34099d14c4967e441e
Andrew Ligon <andrew.ligon@nextthought.com>
2016-09-06 15:29:00 -0500

    Reject the conflict on cancel

--- src/conflict-resolution/default-handler.js
+++ src/conflict-resolution/default-handler.js
@@ -3,11 +3,11 @@ import {modal} from '../prompts';
 import DefaultConfirmPrompt from './components/DefaultConfirmPrompt';
 
 export default function defaultConflictHandler (challenge) {
-	return new Promise(confirm => {
+	return new Promise((confirm, reject) => {
 		modal(
-			<DefaultConfirmPrompt challenge={challenge} onConfirm={confirm} />,
+			<DefaultConfirmPrompt challenge={challenge} onConfirm={confirm} onCancel={reject} />,
 			'request-conflict-resolver'
 		);
 	})
-		.then(() => challenge.confirm());
+		.then(() => challenge.confirm(), () => challenge.reject());
 }
