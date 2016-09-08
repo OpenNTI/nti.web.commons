a737aa902804a821c210088ad2b09506eaa4b007
Josh Birdwell <jbirdwell34@gmail.com>
2016-09-08 11:10:18 -0500

    Create native select

---
+++ src/components/Select.jsx
@@ -0,0 +1,45 @@
+import React, {PropTypes} from 'react';
+import classNames from 'classnames';
+
+export default function Select (props) {
+	const {value, onChange, className, children, empty, ...otherProps} = props;
+	const selectClassNames = classNames('nti-select-native', className);
+
+	const selected = React.Children.toArray(children)
+					.find(child => (
+						child = child.props,
+						React.Children.count(child.children) > 0 && child.value === value
+					));
+
+	let valueLabel = value;
+
+	if (selected) {
+		let {props: {children: selectedLabel}} = selected;
+		if (Array.isArray(selectedLabel)) {
+			selectedLabel = selectedLabel[0];
+		}
+
+		if (selectedLabel) {
+			valueLabel = selectedLabel;
+		}
+	}
+
+	return (
+		<div className="nti-select-native-wrapper">
+			<span className="icon-chevron-down small"/>
+			<span className="nti-select-value">{valueLabel}</span>
+			<select className={selectClassNames} {...otherProps} value={empty && value == null ? 'empty' : value} onChange={onChange}>
+				{(!value && empty) && <option disabled value="empty"></option>}
+				{children}
+			</select>
+		</div>
+	);
+}
+
+Select.propTypes = {
+	value: PropTypes.number,
+	onChange: PropTypes.func.isRequired,
+	children: PropTypes.array.isRequired,
+	className: PropTypes.string,
+	empty: PropTypes.bool
+};
