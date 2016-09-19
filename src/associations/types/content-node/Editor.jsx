9b892fb9d948dae84a0c5c612a00e6c46c08fc71
Andrew Ligon <andrew.ligon@nextthought.com>
2016-09-19 12:55:10 -0500

    Fill out the sub label for content nodes

--- src/associations/types/content-node/Editor.jsx
+++ src/associations/types/content-node/Editor.jsx
@@ -4,6 +4,28 @@ import ItemInfo from '../components/ItemInfo';
 import AddButton from '../components/AddButton';
 import RemoveButton from '../components/RemoveButton';
 
+import DateTime from '../../../components/DateTime';
+
+
+function getSubLabels (item, isActive) {
+	const labels = [];
+
+	if (isActive) {
+		labels.push(item.group.label);
+	}
+
+	if (!item.item.isPublished()) {
+		labels.push('Draft');
+	} else if (item.item.AvailableBeginning) {
+		labels.push(`Scheduled for ${DateTime.format(item.item.getAvailableBeginning(), 'll')}`);
+	} else {
+		labels.push('Published');
+	}
+
+	return labels;
+}
+
+
 ContentNodeEditor.propTypes = {
 	item: React.PropTypes.object,
 	associations: React.PropTypes.object
@@ -21,7 +43,7 @@ export default function ContentNodeEditor ({item, associations}) {
 
 	return (
 		<ListItem className="content-node" active={active}>
-			<ItemInfo label={item.label} subLabels={[item.group.label, 'Test label']}/>
+			<ItemInfo label={item.label} subLabels={getSubLabels(item, active)}/>
 			{!active && item.canAddTo && (<AddButton onAdd={onAdd} />)}
 			{active && item.canRemoveFrom && (<RemoveButton onRemove={onRemove} />)}
 		</ListItem>
