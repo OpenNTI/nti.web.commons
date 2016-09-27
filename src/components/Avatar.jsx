d4c07dfceb7e8514db8414f5ea7abb59169f77c1
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-09-27 13:28:13 -0500

    copy over Avatar & DisplayName components

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import cx from 'classnames';
+
+import {resolve, getDebugUsernameString} from 'nti-web-client/lib/user';
+
+import Constants from '../constants';
+
+const {DataURIs: {BLANK_AVATAR, BLANK_GROUP_AVATAR}} = Constants;
+
+const DEFAULT = { entity: {avatarURL: BLANK_AVATAR }};
+const DEFAULT_GROUP = { entity: {avatarURL: BLANK_GROUP_AVATAR }};
+
+
+export default class Avatar extends React.Component {
+
+	static propTypes = {
+		entity: React.PropTypes.oneOfType([
+			React.PropTypes.object,
+			React.PropTypes.string
+		]),
+
+		className: React.PropTypes.string
+	}
+
+
+	constructor (props) {
+		super(props);
+		this.state = {};
+	}
+
+
+	componentDidMount () {
+		this.fillIn();
+	}
+
+
+	componentWillReceiveProps (nextProps) {
+		if (this.props.entity !== nextProps.entity) {
+			this.fillIn(nextProps);
+		}
+	}
+
+
+	fillIn (props = this.props) {
+
+		this.setState({loading: true});
+
+		resolve(props)
+			.catch(() => DEFAULT)
+			.then(x => this.setState({
+				entity: x,
+				color: this.getColorClass(x),
+				loading: false
+			}));
+	}
+
+
+	getColorClass (entity) {
+
+		function hash (str) {
+			let h = 0, c;
+			if (str.length === 0) {
+				return h;
+			}
+
+			for (let i = 0; i < str.length; i++) {
+				c = str.charCodeAt(i);
+				/*eslint-disable no-bitwise */
+				h = ((h << 5) - h) + c;
+				h = h & h; // Convert to 32bit integer
+				/*eslint-enable no-bitwise */
+			}
+			return h;
+		}
+
+		const NUM_COLORS = 12;
+
+		let hashedString = (typeof entity === 'string'
+								? entity
+								: (entity || {}).Username) || 'unknown';
+
+		let idx = Math.abs(hash(hashedString)) % NUM_COLORS;
+
+		return `avatar-color-${idx}`;
+	}
+
+
+	isGroup () {
+		return /\..*(friendslist|community)/i.test((this.state.entity || {}).MimeType);
+	}
+
+
+	setUnknown () {
+		if (!this.mounted) {
+			return;
+		}
+		this.setState(this.isGroup() ? DEFAULT_GROUP : DEFAULT);
+	}
+
+
+	fallback () {
+		return this.isGroup() ? BLANK_GROUP_AVATAR : BLANK_AVATAR;
+	}
+
+
+	render () {
+		const {loading, entity, color} = this.state;
+		const {className} = this.props;
+
+		if (loading) { return null; }
+
+		const {avatarURL, initials, displayName} = entity || {};
+
+		const childProps = {
+			...this.props,
+			'data-for': getDebugUsernameString(entity),
+			alt: 'Avatar for ' + displayName,
+			className: cx('avatar', color, className)
+		};
+
+		delete childProps.entity;
+
+		return avatarURL ? (
+				<img {...childProps} src={avatarURL} onError={this.setUnknown}/>
+			) : initials ? (
+				<svg {...childProps} viewBox="0 0 32 32">
+					<text textAnchor="middle" x="16px" y="21px">{initials}</text>
+				</svg>
+			) : (
+				<img {...childProps} src={this.fallback()}/>
+			);
+	}
+}
---
+++ src/components/DisplayName.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import cx from 'classnames';
+
+import t from 'nti-lib-locale';
+import {getAppUsername} from 'nti-web-client';
+import {resolve, getDebugUsernameString} from 'nti-web-client/lib/user';
+
+
+/**
+ * This DisplayName component can use the full Entity instance if you have it.
+ * Otherwise, it will take a username string for the entity prop. If you do not
+ * have the full entity object, and you want to show the display name, do not
+ * resolve the full entity object yourself just to pass to this componenent.
+ * Only resolve the entity IF and ONLY IF you need it for something else. Most
+ * likely, if its a link, or something, use the corresponding Component,
+ * do not roll your own.
+ */
+export default class DisplayName extends React.Component {
+
+	static propTypes = {
+		className: React.PropTypes.string,
+
+		localeKey: React.PropTypes.string,
+
+		tag: React.PropTypes.any,
+
+		entity: React.PropTypes.oneOfType([
+			React.PropTypes.object,
+			React.PropTypes.string
+		]).isRequired,
+
+		/**
+		 * Specifies to substitute your name with "You".
+		 *
+		 * @type {boolean}
+		 */
+		usePronoun: React.PropTypes.bool,
+
+		/**
+		 * Sharing Scopes (entity objects) are given GeneralNames by the suggestion provider.
+		 * This flag will instruct this component to use that designation instead of the displayName.
+		 *
+		 * @type {boolean}
+		 */
+		useGeneralName: React.PropTypes.bool
+	}
+
+
+	constructor (props) {
+		super(props);
+		this.state = {
+			displayName: ''
+		};
+	}
+
+
+	componentDidMount () { this.fillIn(); }
+
+	componentWillReceiveProps (nextProps) {
+		let {entity} = this.props;
+		if (entity !== nextProps.entity) {
+			this.fillIn(nextProps);
+		}
+	}
+
+
+	fillIn (props = this.props) {
+		let appuser = getAppUsername();
+		let {usePronoun} = props;
+		let task = Date.now();
+
+		let set = state => {
+			if (this.state.task === task) {
+				this.setState(state);
+			}
+		};
+
+		this.setState({task}, ()=> resolve(props)
+			.then(
+				entity => {
+					let displayName = (usePronoun && entity.getID() === appuser)
+						? 'You'
+						: entity.displayName;
+
+					let { generalName } = entity;
+
+					set({ displayName, generalName });
+				},
+				()=> set({ failed: true, displayName: 'Unknown' })
+			));
+
+	}
+
+
+	render () {
+		const {
+			props: {className, entity, localeKey, tag, useGeneralName,...otherProps},
+			state: {displayName, generalName}
+		} = this;
+
+		const Tag = tag || (localeKey ? 'address' : 'span');
+		let name = (useGeneralName && generalName) || displayName;
+
+		const props = {
+			...otherProps,
+			className: cx('username', className),
+			children: name,
+			'data-for': getDebugUsernameString(entity)
+		};
+
+		delete props.usePronoun;
+
+		if (localeKey) {
+			name = ReactDOMServer.renderToStaticMarkup(<a rel="author" className="username">{name}</a>);
+
+			Object.assign(props, {
+				children: void 0,
+				dangerouslySetInnerHTML: {'__html': t(localeKey, {name})}
+			});
+		}
+
+		return <Tag {...props} rel="author"/>;
+	}
+}
