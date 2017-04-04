d80e15e74033f6b8cf6f22fdeb4d91a7e6143230
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-04 11:19:16 -0500

    Add a disabled prop to the button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,12 +6,13 @@ Button.propTypes = {
 	className: React.PropTypes.string,
 	children: React.PropTypes.node,
 	rounded: React.PropTypes.bool,
-	onClick: React.PropTypes.func
+	onClick: React.PropTypes.func,
+	disabled: React.PropTypes.bool,
 };
 export default function Button (props) {
-	const {className, children, rounded, onClick = () => {}, ...otherProps} = props;
+	const {className, children, rounded, disabled, onClick = () => {}, ...otherProps} = props;
 	//Add "basic" as a class, it can be removed if we need to add other style variations
-	const cls = cx(className, 'basic-button', {rounded, primary: true});
+	const cls = cx(className, 'basic-button', {rounded, primary: true, disabled});
 
 	return (
 		<a {...otherProps}
