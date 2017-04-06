ab619cd3695393461aff6e642e5e27f1c2c94360
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-06 11:36:53 -0500

    Style the text input

--- src/components/inputs/Text.js
+++ src/components/inputs/Text.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import cx from 'classnames';
 
+
+const stop = e => e.preventDefault();
+
 export default class TextInput extends React.Component {
 	static propTypes = {
 		className: React.PropTypes.string,
@@ -11,6 +14,11 @@ export default class TextInput extends React.Component {
 	}
 
 	attachInputRef = x => this.input = x;
+
+
+	get validity () {
+		return this.input.validity;
+	}
 
 
 	focus () {
@@ -35,19 +43,24 @@ export default class TextInput extends React.Component {
 		if (onChange) {
 			onChange('');
 		}
+
+		this.focus();
 	}
 
 
 	render () {
 		const {value, label, className, disableClear, ...otherProps} = this.props;
-		const cls = cx('nti-text-input', className);
+		const cls = cx('nti-text-input', className, {'disable-clear': disableClear});
 
+		delete otherProps.onChange;
 
 		return (
 			<label className={cls}>
 				{label && (<span className="label">{label}</span>)}
-				<input type="text" value={value} onChange={this.onChange} ref={this.attachInputRef} {...otherProps} />
-				{!disableClear && (<i className="icon-light-x" onClick={this.onClear} />)}
+				<span className="input-container">
+					<input type="text" value={value} onChange={this.onChange} ref={this.attachInputRef} {...otherProps} />
+					{!disableClear && (<div className="reset" onClick={this.onClear} onMouseDown={stop} />)}
+				</span>
 			</label>
 		);
 	}
