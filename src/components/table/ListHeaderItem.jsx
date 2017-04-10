0d8ee0ddfc1e266f21c85fc62edbebd229cfc43d
Cody Crow <cody.crow@nextthought.com>
2017-04-10 16:35:26 -0500

    NTI-3117 Refactor Table.ListTable around rows/columns

--- src/components/table/ListHeaderItem.jsx
+++ src/components/table/ListHeaderItem.jsx
@@ -7,43 +7,54 @@ import {ASCENDING, DESCENDING} from './Constants';
 
 export default class ListHeaderCell extends React.Component {
 	static propTypes = {
-		cell: PropTypes.shape({
-			name: PropTypes.string.isRequired,
-			className: PropTypes.string,
-			display: PropTypes.string,
-			sortFn: PropTypes.func.isRequired
+		column: React.PropTypes.shape({
+			name: React.PropTypes.string.isRequired,
+			classes: React.PropTypes.shape({
+				name: React.PropTypes.string,
+				inactive: React.PropTypes.string,
+				active: React.PropTypes.string,
+				asc: React.PropTypes.string,
+				desc: React.PropTypes.string
+			}),
+			display: React.PropTypes.string,
+			sortFn: React.PropTypes.func.isRequired
 		}),
-		active: PropTypes.bool,
-		direction: PropTypes.string,
-		onSort: PropTypes.func,
+		active: React.PropTypes.bool,
+		direction: React.PropTypes.string,
+		onSort: React.PropTypes.func
 	}
 
-
 	onClick = () => {
-		const {cell, active, direction, onSort} = this.props;
+		const {column, direction, onSort} = this.props;
 
-		const newDirection = !active || direction === ASCENDING ? DESCENDING : ASCENDING;
+
+		const newDirection = direction === ASCENDING ? DESCENDING : ASCENDING;
 		const directionMod = direction === ASCENDING ? -1 : 1;
 
 		const sortFn = (a, b) => {
-			const sort = cell.sortFn(a, b);
+			const sort = column.sortFn(a, b);
 
 			return sort * directionMod;
 		};
 
 		if (onSort) {
-			onSort(sortFn, cell.name, newDirection);
+			onSort(sortFn, column.name, newDirection);
 		}
 	}
 
 
 	render () {
-		const {cell, active, direction} = this.props;
-		const cls = cx('list-header-cell', cell.className, active ? direction : '', {active});
+		const {column, active, direction} = this.props;
+		const cls = cx('list-header-column', column.classes.name, {
+			[`${column.classes.inactive}`]: !active,
+			[`${column.classes.active}`]: active,
+			[`${column.classes.asc}`]: active && direction === ASCENDING,
+			[`${column.classes.desc}`]: active && direction === DESCENDING
+		});
 
 		return (
 			<div className={cls} onClick={this.onClick}>
-				<span>{cell.display || cell.name}</span>
+				<span>{column.display || column.name}</span>
 			</div>
 		);
 	}
