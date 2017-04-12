3a11c8e2878823ae6fcb488345fb7387f4073a20
Cody Crow <cody.crow@nextthought.com>
2017-04-12 14:15:05 -0500

    NTI-3117 Update ListTable to React 15.5-style PropTypes

--- src/components/table/ListHeader.jsx
+++ src/components/table/ListHeader.jsx
@@ -7,23 +7,23 @@ import ListHeaderItem from './ListHeaderItem';
 
 export default class ListHeader extends React.Component {
 	static propTypes = {
-		className: React.PropTypes.string,
-		columns: React.PropTypes.arrayOf(React.PropTypes.shape({
-			name: React.PropTypes.string.isRequired,
-			classes: React.PropTypes.shape({
-				name: React.PropTypes.string,
-				default: React.PropTypes.string,
-				inactive: React.PropTypes.string,
-				active: React.PropTypes.string,
-				asc: React.PropTypes.string,
-				desc: React.PropTypes.string
+		className: PropTypes.string,
+		columns: PropTypes.arrayOf(PropTypes.shape({
+			name: PropTypes.string.isRequired,
+			classes: PropTypes.shape({
+				name: PropTypes.string,
+				default: PropTypes.string,
+				inactive: PropTypes.string,
+				active: PropTypes.string,
+				asc: PropTypes.string,
+				desc: PropTypes.string
 			}),
-			display: React.PropTypes.string,
-			sortFn: React.PropTypes.func.isRequired
+			display: PropTypes.string,
+			sortFn: PropTypes.func.isRequired
 		})),
-		activeSort: React.PropTypes.string,
-		activeDirection: React.PropTypes.string,
-		onSortChange: React.PropTypes.func
+		activeSort: PropTypes.string,
+		activeDirection: PropTypes.string,
+		onSortChange: PropTypes.func
 	}
 
 
