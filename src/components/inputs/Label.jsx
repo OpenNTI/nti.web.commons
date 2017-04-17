abc014041ea33440a9636bb53907ee1a11a8809a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-04-17 13:14:39 -0500

    cloneElement does not auto-chain refs
    > https://github.com/facebook/react/issues/4296
    
    You need to maintain the original ref... cloneElement does not auto-chain them. Use `getRefHandler` from `nti-commons`
    
    ```js
    React.cloneElement(child, ref: getRefHandler(child.ref, this.attachInputRef))
    ```

--- src/components/inputs/Label.jsx
+++ src/components/inputs/Label.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import {getRefHandler} from 'nti-commons';
 
 
 export default class LabeledInput extends React.Component {
@@ -45,6 +46,6 @@ export default class LabeledInput extends React.Component {
 		const {children} = this.props;
 		const child = React.Children.only(children);
 
-		return React.cloneElement(child, {ref: this.attachInputRef});
+		return React.cloneElement(child, {ref: getRefHandler(child.ref, this.attachInputRef)});
 	}
 }
