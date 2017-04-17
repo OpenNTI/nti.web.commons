a426f2e8ba056491f2fe8ab846cd6466faca3306
Andrew Ligon <andrew.ligon@nextthought.com>
2017-04-17 15:15:52 -0500

    Add tests for clearable

---
+++ src/components/inputs/__test__/Clearable.spec.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {mount} from 'enzyme';
+
+import Clearable from '../Clearable';
+import Text from '../Text';
+
+describe('Clearable Inputs', () => {
+
+	function buildProps (props) {
+		const newProps = {...props, onChange: () => {}};
+
+		spyOn(newProps, 'onChange');
+
+		return newProps;
+	}
+
+	it('No onClear calls onChange with null on the input', () => {
+		const inputProps = buildProps({});
+		const clearProps = buildProps({});
+
+		const wrapper = mount(<Clearable {...clearProps} ><Text {...inputProps} /></Clearable>);
+		const clearButton = wrapper.find('.reset');
+
+		clearButton.simulate('click');
+
+		expect(inputProps.onChange).toHaveBeenCalledWith(null);
+	});
+
+	it('onClear is called when present and onChange is not', () => {
+		const inputProps = buildProps({});
+		const clearProps = buildProps({onClear: () => {}});
+
+		spyOn(clearProps, 'onClear');
+
+		const wrapper = mount(<Clearable {...clearProps} ><Text {...inputProps} /></Clearable>);
+		const clearButton = wrapper.find('.reset');
+
+		clearButton.simulate('click');
+
+		expect(clearProps.onClear).toHaveBeenCalled();
+		expect(inputProps.onChange).not.toHaveBeenCalled();
+	});
+});
