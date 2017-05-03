6f3dafc87d78b056869acf10a74b792a7e2c196b
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-05-03 10:21:06 -0500

    Update to DeauthListener. 1) update DeauthListener to support functions in code map. 2) remove 403's from map, now that we know a 401 won't show the prompt unless there is a WWW-Authenticate header in the response.

--- src/components/DeauthListener.jsx
+++ src/components/DeauthListener.jsx
@@ -8,7 +8,8 @@ const logger = Logger.get('nti:commons:DeauthListener');
 
 const DEAUTH_CODES = {
 	401: true,
-	403: true,
+	//Example of checking a status code...(returning true means request is not authenticated and needs to re-login)
+	// 403: (response) => response.headers.has('WWW-Authenticate')
 };
 
 export default class DeauthListener extends React.Component {
@@ -33,7 +34,11 @@ export default class DeauthListener extends React.Component {
 
 	onError = (err) => {
 		const {basePath} = this.props;
-		if (DEAUTH_CODES[err.statusCode] && typeof location !== 'undefined') {
+		const AuthCheck = DEAUTH_CODES[err.statusCode];
+
+		const hasBeenDeauthorized = typeof AuthCheck === 'function' ? AuthCheck(err.response) : AuthCheck;
+
+		if (hasBeenDeauthorized && typeof location !== 'undefined') {
 			logger.info('XHR recieved an `%d: Not Authorized`, needs reauth. Redirecting to: %s', err.statusCode, basePath);
 			location.replace(basePath);
 		}
