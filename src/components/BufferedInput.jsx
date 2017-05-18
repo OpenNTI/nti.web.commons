315822a6765f9cdd7c8b732a90348824dde86a3f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-05-18 12:14:26 -0500

    Deprecate BufferedInput.

--- src/components/BufferedInput.jsx
+++ src/components/BufferedInput.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * @deprecated - There are a few ideological problems with this component.
+ * 1) The onChange is delayed/buffered. (I know thats the intent, but thats not where the buffer should be)
+ * 2) There is no "prop-change" way to change the value. This breaks the React paradigm.
+ */
+
 export default class BufferedInput extends React.Component {
 	static propTypes = {
 		defaultValue: PropTypes.string,
@@ -9,6 +15,12 @@ export default class BufferedInput extends React.Component {
 	}
 
 	attachRef = x => this.input = x
+
+
+	componentDidMount () {
+		//eslint-disable-next-line no-console
+		console.warn('BufferedInput is deprecated. Please attempt to shift to a regular input and buffer the method onChange calls.');
+	}
 
 
 	clear () {
