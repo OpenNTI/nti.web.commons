OpenNTI/nti.web.commons

commit 01fcd819c6c1e11e2336ea91ff79c0a90e2ea30f

allow selected option to have complex label

src/components/Select.jsx
@@ -15,13 +15,9 @@ export default function Select (props) {
 	let valueLabel = value;
 
 	if (selected) {
-		let {props: {children: selectedLabel}} = selected;
-		if (Array.isArray(selectedLabel)) {
-			selectedLabel = selectedLabel[0];
-		}
-
-		if (selectedLabel) {
-			valueLabel = selectedLabel;
+		const labelElements = React.Children.map(selected.props.children, React.cloneElement);
+		if (labelElements) {
+			valueLabel = labelElements;
 		}
 	}
 

commit cfb450fc95696eb9a26f818187ce07d896d9fc70

fix an issue introduced by 01fcd819 that caused Select to blow up on string option labels
The feature allowing HTML labels is of questionable value given that the html option element itself doesn't allow it. So the vanity label renders correctly but the option itself doesn't.

src/components/Select.jsx
@@ -15,7 +15,7 @@ export default function Select (props) {
 	let valueLabel = value;
 
 	if (selected) {
-		const labelElements = React.Children.map(selected.props.children, React.cloneElement);
+		const labelElements = React.Children.map(selected.props.children, (c) => typeof c !== 'object' ? c : React.cloneElement(c));
 		if (labelElements) {
 			valueLabel = labelElements;
 		}