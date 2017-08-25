e191c39acac6d8feaad613a848749b1c9b4e8bda
Cory Jones <cory.jones@nextthought.com>
2017-08-25 08:19:00 -0500

    NTI-3867 Listen for msg bar open/closed events

--- src/components/StickyElement.jsx
+++ src/components/StickyElement.jsx
@@ -2,29 +2,63 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Sticky as ReactSticky} from 'react-sticky';
 
+import getEmitter from '../utils/events';
+
 const offsetProp = 'nti-sticky-top-offset';
 
-Sticky.propTypes = {
-	children: PropTypes.node
-};
-
-export default function Sticky ({children}) {
-	const offset = window && window[offsetProp];
-	let topOffset = 0;
-
-	if (offset) {
-		topOffset = -offset;
+export default class Sticky extends React.Component {
+	static propTypes = {
+		children: PropTypes.node
 	}
 
-	return (
-		<ReactSticky topOffset={topOffset}>
-			{({style}) => {
-				if (offset && style.top != null && style.top === 0) {
-					style.top += offset;
-				}
+	constructor (props) {
+		super(props);
 
-				return (<div className="sticky" style={style}>{children}</div>);
-			}}
-		</ReactSticky>
-	);
+		this.state = this.getOffset();
+	}
+
+	componentDidMount () {
+		getEmitter().addListener('msg-bar-opened', () => { this.updateOffset(); });
+		getEmitter().addListener('msg-bar-closed', () => { this.updateOffset(); });
+	}
+
+	componentWillUnmount () {
+		getEmitter().removeListener('msg-bar-opened');
+		getEmitter().removeListener('msg-bar-closed');
+	}
+
+	updateOffset () {
+		this.setState(this.getOffset());
+	}
+
+	getOffset () {
+		const offset = window && window[offsetProp]();
+		let topOffset = 0;
+
+		if (offset) {
+			topOffset = -offset;
+		}
+
+		return {
+			topOffset,
+			offset
+		};
+	}
+
+	render () {
+		const { children } = this.props;
+		const { offset, topOffset } = this.state;
+
+		return (
+			<ReactSticky topOffset={topOffset}>
+				{({style}) => {
+					if (offset && style.top != null && style.top === 0) {
+						style.top += offset;
+					}
+
+					return (<div className="sticky" style={style}>{children}</div>);
+				}}
+			</ReactSticky>
+		);
+	}
 }
