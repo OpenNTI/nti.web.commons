c1b1a675f1aeb6c534e2bd1ba8b1da6fc11235ea
Josh Birdwell <jbirdwell34@gmail.com>
2017-09-15 11:23:37 -0500

    Export panels components

--- src/components/panels/index.js
+++ src/components/panels/index.js
@@ -1,2 +1,6 @@
 export MessageBar from './MessageBar';
 export TitleBar from './TitleBar';
+export Header, { TitleBalencer as HeaderSpacer }  from './Header';
+export Toolbar, { Spacer as ToolbarSpacer }  from './Toolbar';
+export ToolbarButton from './ToolbarButton';
+export ToolbarButtonGroup from './ToolbarButtonGroup';
