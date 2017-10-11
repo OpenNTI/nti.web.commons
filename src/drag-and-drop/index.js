f21098a702ef455e7b3649e8cc8e4e5eb36ef421
Ray Hatfield <ray.hatfield@nextthought.com>
2017-10-10 19:13:27 -0500

    update exports

--- src/drag-and-drop/index.js
+++ src/drag-and-drop/index.js
@@ -1,4 +1,3 @@
 export Draggable from './Draggable';
-export Inert from './sortable/Inert';
-export LockedItem from './sortable/LockedItem';
 export Sortable from './sortable/Container';
+export Item from './sortable/Item';
