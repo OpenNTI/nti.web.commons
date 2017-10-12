3a805c8eeafe7a669b46496d715b1dd100124125
Unknown <cory.jones@nextthought.com>
2017-10-12 13:06:17 -0500

    NTI-4153 Use restoreScroll option so dialog doesn't cause a jump

--- src/conflict-resolution/content-version-conflict.js
+++ src/conflict-resolution/content-version-conflict.js
@@ -10,9 +10,11 @@ export default function contentVersionConflictHandler (challenge) {
 	return new Promise((confirm, reject) => {
 		modal(
 			<ContentVersionConflictPrompt challenge={challenge} onConfirm={confirm} onCancel={reject} />,
-			'request-conflict-resolver'
+			{
+				className: 'request-conflict-resolver',
+				restoreScroll: true
+			}
 		);
 	})
 		.then((...args) => challenge.confirm(...args), () => challenge.reject());
 }
-
