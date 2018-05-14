b5a6dd1d9dfb89d81bda12cb37c2f1825e5a38c6
Cory Jones <cory.jones@nextthought.com>
2018-05-14 16:28:32 -0500

    NTI-5699 Convert LabeledValue to pure comp, implement getDOMNode

--- src/components/LabeledValue.jsx
+++ src/components/LabeledValue.jsx
@@ -2,27 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-LabeledValue.propTypes = {
-	label: PropTypes.any,
-	children: PropTypes.any,
-	className: PropTypes.string,
-	arrow: PropTypes.bool,
-	disabled: PropTypes.bool
-};
+export default class LabeledValue extends React.PureComponent {
+	static propTypes = {
+		label: PropTypes.any,
+		children: PropTypes.any,
+		className: PropTypes.string,
+		arrow: PropTypes.bool,
+		disabled: PropTypes.bool
+	};
 
-export default function LabeledValue (props) {
-	const {arrow, children, className, label, disabled, ...otherProps} = props;
-	const classes = cx('labeled-value', className, {
-		'arrow-down': arrow,
-		disabled
-	});
+	domNode = React.createRef();
 
-	return (
-		<div {...otherProps} className={classes}>
-			<label>{label}</label>
-			<div className="value">
-				{children}
+	getDOMNode () {
+		return this.domNode.current;
+	}
+
+	render () {
+		const {arrow, children, className, label, disabled, ...otherProps} = this.props;
+		const classes = cx('labeled-value', className, {
+			'arrow-down': arrow,
+			disabled
+		});
+
+		return (
+			<div {...otherProps} className={classes} ref={this.domNode}>
+				<label>{label}</label>
+				<div className="value">
+					{children}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
