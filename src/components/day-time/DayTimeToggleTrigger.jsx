289d8cdccefd9ea67281fac162967347a0af97e6
Cory Jones <cory.jones@nextthought.com>
2018-05-21 16:51:33 -0500

    NTI-5773 Convert DayTimeToggleTrigger and and add getDOMNode

--- src/components/day-time/DayTimeToggleTrigger.jsx
+++ src/components/day-time/DayTimeToggleTrigger.jsx
@@ -4,35 +4,43 @@ import cx from 'classnames';
 
 import DateTime from '../DateTime';
 
-DayTimeToggleTrigger.propTypes = {
-	className: PropTypes.string,
-	availableBeginning: PropTypes.instanceOf(Date),
-	availableEnding: PropTypes.instanceOf(Date),
-	onChange: PropTypes.func,
-	disableText: PropTypes.bool
-};
+export default class DayTimeToggleTrigger extends React.Component {
+	static propTypes = {
+		className: PropTypes.string,
+		availableBeginning: PropTypes.instanceOf(Date),
+		availableEnding: PropTypes.instanceOf(Date),
+		onChange: PropTypes.func,
+		disableText: PropTypes.bool
+	}
 
-export default function DayTimeToggleTrigger (props) {
-	const {availableBeginning: begin, availableEnding: end, onChange, className, disableText, ...otherProps} = props;
+	domNode = React.createRef()
 
-	const dateClassNames = cx('calendar', {empty: !begin});
-	const clearClassNames = cx('clear', {empty: !begin});
-	const mainTextClassNames = cx('text', {hasDate: begin}, {hide: disableText});
+	getDOMNode () {
+		return this.domNode.current;
+	}
 
-	const mainText = !begin && !end ? 'When should students begin this lesson?' : begin && end ? `${DateTime.format(begin, 'MMMM D')} - ${DateTime.format(end, 'MMMM D')}` : `${DateTime.format(begin, 'dddd, MMMM Do, YYYY')}`;
+	render () {
+		const {availableBeginning: begin, availableEnding: end, onChange, className, disableText, ...otherProps} = this.props;
 
-	return (
-		<div {...otherProps} className={cx('daytime-toggle-trigger', className)}>
-			<div className={dateClassNames}>
-				<div className="month">{DateTime.format(begin, 'MMM')}</div>
-				<div className="day">{DateTime.format(begin, 'D')}</div>
-			</div>
-			<div className="main">
-				<div className={mainTextClassNames}>
-					{mainText}
-					<div className={clearClassNames} onClick={onChange}>Clear</div>
+		const dateClassNames = cx('calendar', {empty: !begin});
+		const clearClassNames = cx('clear', {empty: !begin});
+		const mainTextClassNames = cx('text', {hasDate: begin}, {hide: disableText});
+
+		const mainText = !begin && !end ? 'When should students begin this lesson?' : begin && end ? `${DateTime.format(begin, 'MMMM D')} - ${DateTime.format(end, 'MMMM D')}` : `${DateTime.format(begin, 'dddd, MMMM Do, YYYY')}`;
+
+		return (
+			<div {...otherProps} className={cx('daytime-toggle-trigger', className)} ref={this.domNode}>
+				<div className={dateClassNames}>
+					<div className="month">{DateTime.format(begin, 'MMM')}</div>
+					<div className="day">{DateTime.format(begin, 'D')}</div>
+				</div>
+				<div className="main">
+					<div className={mainTextClassNames}>
+						{mainText}
+						<div className={clearClassNames} onClick={onChange}>Clear</div>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
