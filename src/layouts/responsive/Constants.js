265435c279f07bb394e1eeb961f5bee7eb17c07d
Andrew Ligon <andrew.ligon@nextthought.com>
2018-05-22 12:31:24 -0500

    Don't require an argument for isMobile, isTablet, isDesktop

--- src/layouts/responsive/Constants.js
+++ src/layouts/responsive/Constants.js
@@ -1,9 +1,10 @@
 import {isScreenWidthInRange} from './utils';
 
 //The ranges are taken from $responsive-ranges in @nti/style-common/responsive.scss
-export const isMobile = ({screenSize}) => isScreenWidthInRange(screenSize, 0, 480);
-export const isTablet = ({screenSize}) => isScreenWidthInRange(screenSize, 481, 1024);
-export const isDesktop = ({screenSize}) => isScreenWidthInRange(screenSize, 1025, Infinity);
+export const isMobile = () => isScreenWidthInRange(0, 480);
+export const isMobile = () => isScreenWidthInRange(0, 480);
+export const isTablet = () => isScreenWidthInRange(481, 1024);
+export const isDesktop = () => isScreenWidthInRange(1025, Infinity);
 
 let CONTEXT_FLAG = null;
 const WEBAPP_CONTEXT = 'webapp';
