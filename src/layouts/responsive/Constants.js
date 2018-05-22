d93194aa1d017bf8dca0ad445a168591d888c2c0
Andrew Ligon <andrew.ligon@nextthought.com>
2018-05-22 12:41:41 -0500

    typos

--- src/layouts/responsive/Constants.js
+++ src/layouts/responsive/Constants.js
@@ -2,7 +2,6 @@ import {isScreenWidthInRange} from './utils';
 
 //The ranges are taken from $responsive-ranges in @nti/style-common/responsive.scss
 export const isMobile = () => isScreenWidthInRange(0, 480);
-export const isMobile = () => isScreenWidthInRange(0, 480);
 export const isTablet = () => isScreenWidthInRange(481, 1024);
 export const isDesktop = () => isScreenWidthInRange(1025, Infinity);
 
