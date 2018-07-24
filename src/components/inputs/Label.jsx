d915731726a98410d45cdbc69cd4bd7882b327c7
Jonathan Grimes <jsg2021@gmail.com>
2018-07-24 10:31:44 -0500

    dedupe classNames in Input.Label

--- src/components/inputs/Label.jsx
+++ src/components/inputs/Label.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
+import cx from 'classnames/dedupe';
 import {getRefHandler} from '@nti/lib-commons';
 
 
