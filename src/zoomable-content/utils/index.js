54fc34590b31a4f8106b93ac2499b52659f7eb73
Andrew Ligon <andrew.ligon@nextthought.com>
2018-11-20 14:46:44 -0600

    remove unused utilities

--- src/zoomable-content/utils/index.js
+++ src/zoomable-content/utils/index.js
@@ -1,8 +1,2 @@
-export createTransform from './create-transform';
 export getLimitingSide from './get-limiting-side';
 export getScaleForMouseWheel from './get-scale-for-mouse-wheel';
-export getStyleForTransform from './get-style-for-transform';
-export fixTransform from './fix-transform';
-export isValidTransform from './is-valid-transform';
-export * as Scale from './scale';
-export * as Translation from './translation';
