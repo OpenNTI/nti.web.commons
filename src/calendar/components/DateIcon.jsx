246d377ddf5737202f898948887b36de9388732d
Cory Jones <cory.jones@nextthought.com>
2018-12-05 12:52:04 -0600

    Support a 'viewed' prop to alter the badge display accordingly

--- src/calendar/components/DateIcon.jsx
+++ src/calendar/components/DateIcon.jsx
@@ -9,17 +9,18 @@ export default class DateIcon extends React.PureComponent {
 	static propTypes = {
 		className: PropTypes.string,
 		date: PropTypes.object,
-		badge: PropTypes.number
+		badge: PropTypes.number,
+		viewed: PropTypes.bool
 	}
 
 	render () {
-		const {date = new Date(), className, badge, ...others} = this.props;
+		const {date = new Date(), className, badge, viewed, ...others} = this.props;
 
 		const badgeProp = badge == null ? {} : {'data-badge': badge};
 		const props = {...badgeProp, ...others};
 
 		return (
-			<div className={cx('nti-calendar-date-icon', className)} {...props}>
+			<div className={cx('nti-calendar-date-icon', className, {viewed})} {...props}>
 				<div className="month">{DateTime.format(date, 'MMM')}</div>
 				<div className="day">{DateTime.format(date, 'D')}</div>
 			</div>
