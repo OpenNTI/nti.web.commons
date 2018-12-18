68e045a8543b504916611abb97f5ff4152897f6f
Ray Hatfield <ray.hatfield@nextthought.com>
2018-12-18 15:02:32 -0600

    workaround for the badge masking part of the small date icon

--- src/calendar/components/DateIcon.jsx
+++ src/calendar/components/DateIcon.jsx
@@ -4,6 +4,11 @@ import cx from 'classnames';
 
 import {DateTime, Badge} from '../../';
 
+// this Small wrapper is part of a workaround for the badge masking the part of the small icon that renders outside the
+// element's actual bounds. see DateIcon.scss for details.
+const Small = props => <div className="nti-calendar-date-icon-small-wrapper" {...props} />;
+const Fragment = ({children}) => children;
+
 export default class DateIcon extends React.PureComponent {
 
 	static propTypes = {
@@ -16,13 +21,16 @@ export default class DateIcon extends React.PureComponent {
 
 	render () {
 		const {date = new Date(), small, className} = this.props;
+		const Wrapper = small ? Small : Fragment;
 
 		return (
-			<Badge position={Badge.POSITIONS.TOP_RIGHT} {...this.props}>
-				<div className={cx('nti-calendar-date-icon', {small}, className)}>
-					<div className="month">{DateTime.format(date, 'MMM')}</div>
-					<div className="day">{DateTime.format(date, 'D')}</div>
-				</div>
+			<Badge position={Badge.POSITIONS.TOP_RIGHT} {...this.props} {...Badge.offset(0, 4)}>
+				<Wrapper>
+					<div className={cx('nti-calendar-date-icon', {small}, className)}>
+						<div className="month">{DateTime.format(date, 'MMM')}</div>
+						<div className="day">{DateTime.format(date, 'D')}</div>
+					</div>
+				</Wrapper>
 			</Badge>
 		);
 	}
