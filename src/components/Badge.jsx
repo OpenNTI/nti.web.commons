9f8ca4da3cbc8d403e853c8a8cfdb88d0986ae87
Ray Hatfield <ray.hatfield@nextthought.com>
2018-12-18 14:29:05 -0600

    support specifying a badge offset; fill in positions

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -4,26 +4,36 @@ import cx from 'classnames';
 
 export default class Badge extends React.Component {
 	static POSITIONS = {
-		TOP_LEFT: cx('top', 'left'),
-		TOP_RIGHT: cx('top', 'right'),
-		BOTTOM_LEFT: cx('bottom', 'left'),
-		BOTTOM_RIGHT: cx('bottom', 'right')
+		TOP_LEFT: 'top-left',
+		TOP_RIGHT: 'top-right',
+		BOTTOM_LEFT: 'bottom-left',
+		BOTTOM_RIGHT: 'bottom-right'
 	};
+
+	static offset = (x, y) => ({offset: {x, y}})
 
 	static propTypes = {
 		viewed: PropTypes.bool,
 		badge: PropTypes.number,
 		position: PropTypes.oneOf(Badge.POSITIONS.TOP_LEFT, Badge.POSITIONS.TOP_RIGHT, Badge.POSITIONS.BOTTOM_LEFT, Badge.POSITIONS.BOTTOM_RIGHT),
-		children: PropTypes.any
+		children: PropTypes.any,
+		offset: PropTypes.shape({
+			x: PropTypes.number,
+			y: PropTypes.number
+		})
 	};
 
 	render () {
-		const {props: {viewed, badge, position, children}} = this;
+		const {props: {viewed, badge, position, children, offset: {x = 0, y = 0} = {}}} = this;
 
 		const positionCls = position || Badge.POSITIONS.TOP_RIGHT;
+		const style = x === 0 && y === 0 ? {} : {
+			...(x !== 0 ? {'--badge-offset-x': `${x}px`} : {}),
+			...(y !== 0 ? {'--badge-offset-y': `${y}px`} : {})
+		};
 
 		const badgeProp = badge > 0 ? {'data-badge': badge > 99 ? '!' : badge} : {};
-		const props = {...badgeProp};
+		const props = {...badgeProp, 'data-badge': 4, style};
 
 		return (
 			<div className={cx('badged-item', positionCls, { viewed })} {...props}>
