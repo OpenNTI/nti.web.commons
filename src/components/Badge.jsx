9dbc728a3c6fcbfe89c0c0ce3a7955651698a1fa
Cory Jones <cory.jones@nextthought.com>
2019-01-16 09:31:51 -0600

    NTI-7184 Add size and theme customization options to Badge component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
+function getDataBadgeValue (raw) {
+	if(typeof raw === 'number') {
+		return raw > 0 ? (raw > 99 ? '!' : raw) : null;
+	}
+
+	return raw;
+}
+
 export default class Badge extends React.Component {
 	static POSITIONS = {
 		TOP_LEFT: 'top-left',
@@ -9,6 +17,17 @@ export default class Badge extends React.Component {
 		CENTER_RIGHT: 'center-right',
 		BOTTOM_LEFT: 'bottom-left',
 		BOTTOM_RIGHT: 'bottom-right'
+	};
+
+	static SIZES = {
+		SMALL: 'small',
+		LARGE: 'large'
+	};
+
+	static THEMES = {
+		ALERT: 'alert',
+		INACTIVE: 'inactive',
+		SUCCESS: 'success'
 	};
 
 	static offset = (x, y) => ({offset: {x, y}})
@@ -20,19 +39,34 @@ export default class Badge extends React.Component {
 			Badge.POSITIONS.TOP_LEFT,
 			Badge.POSITIONS.TOP_RIGHT,
 			Badge.POSITIONS.BOTTOM_LEFT,
-			Badge.POSITIONS.BOTTOM_RIGHT]
-		),
+			Badge.POSITIONS.BOTTOM_RIGHT
+		]),
+		size: PropTypes.oneOf([
+			Badge.SIZES.SMALL,
+			Badge.SIZES.LARGE
+		]),
+		theme: PropTypes.oneOf([
+			Badge.THEMES.ALERT,
+			Badge.THEMES.SUCCESS,
+			Badge.THEMES.INACTIVE
+		]),
 		children: PropTypes.any,
 		offset: PropTypes.shape({
 			x: PropTypes.number,
 			y: PropTypes.number
 		})
-	};
+	}
+
+	static defaultProps = {
+		size: Badge.SIZES.SMALL,
+		theme: Badge.THEMES.ALERT,
+		position: Badge.POSITIONS.TOP_RIGHT
+	}
 
 	render () {
-		const {props: {viewed, badge, position, children, offset: {x = 0, y = 0} = {}}} = this;
+		const {props: {badge, position, theme, size, viewed, children, offset: {x = 0, y = 0} = {}}} = this;
 
-		const positionCls = position || Badge.POSITIONS.TOP_RIGHT;
+		const positionCls = position;
 		const needsStyle = !!badge && (x !== 0 || y !== 0);
 
 		const style = needsStyle ? {
@@ -40,11 +74,18 @@ export default class Badge extends React.Component {
 			...(y !== 0 ? {'--badge-offset-y': `${y}px`} : {})
 		} : {};
 
-		const badgeProp = badge > 0 ? {'data-badge': badge > 99 ? '!' : badge} : {};
+
+		const dataBadgeValue = getDataBadgeValue(badge);
+
+		const badgeProp = dataBadgeValue
+			? {
+				'data-badge': dataBadgeValue
+			}
+			: {};
 		const props = {...badgeProp, style};
 
 		return (
-			<div className={cx('badged-item', positionCls, { viewed })} {...props}>
+			<div className={cx('badged-item', viewed ? Badge.THEMES.INACTIVE : theme, size, 'success', positionCls)} {...props}>
 				<div className="badged-item-content">
 					{children}
 				</div>
