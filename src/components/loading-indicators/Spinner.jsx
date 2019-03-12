15af9c6a89d4abef079ed174cd734629e68d54cf
Andrew Ligon <andrew.ligon@nextthought.com>
2019-03-12 14:30:51 -0500

    Add common props to make a large loading spinner

--- src/components/loading-indicators/Spinner.jsx
+++ src/components/loading-indicators/Spinner.jsx
@@ -2,25 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-LoadingSpinner.propTypes = {
-	white: PropTypes.bool,
-	blue: PropTypes.bool,
-	grey: PropTypes.bool,
-	size: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.number
-	]),
-	strokeWidth: PropTypes.string,
-	className: PropTypes.string
-};
-export default function LoadingSpinner ({className, white, grey, size = '25px', strokeWidth = '5'}) {
-	const cls = cx('loading-spinner', className, {white, grey, blue: !white && !grey});
+export default class LoadingSpinner extends React.PureComponent {
+	static Large = (props) => {
+		return (<LoadingSpinner {...props} size="100px" strokeWidth="2" />);
+	}
 
-	return (
-		<div className={cls} style={{width: size}}>
-			<svg className="circular" viewBox="25 25 50 50">
-				<circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth={strokeWidth} strokeMiterlimit="10"/>
-			</svg>
-		</div>
-	);
+	static propTypes = {
+		white: PropTypes.bool,
+		blue: PropTypes.bool,
+		grey: PropTypes.bool,
+		size: PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.number
+		]),
+		strokeWidth: PropTypes.string,
+		className: PropTypes.string
+	}
+
+	render () {
+		const {className, white, grey, size = '25px', strokeWidth = '5'} = this.props;
+		const cls = cx('loading-spinner', className, {white, grey, blue: !white && !grey});
+
+		return (
+			<div className={cls} style={{width: size}}>
+				<svg className="circular" viewBox="25 25 50 50">
+					<circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth={strokeWidth} strokeMiterlimit="10"/>
+				</svg>
+			</div>
+		);
+	}
 }
