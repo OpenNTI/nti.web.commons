879141b4bb788371dd99d384fdcdc9512c1fad5b
Ray Hatfield <ray.hatfield@nextthought.com>
2019-04-08 17:01:00 -0500

    omit DateIcon Badge wrapper in absence of badge prop; add minimal option

--- src/calendar/components/DateIcon.jsx
+++ src/calendar/components/DateIcon.jsx
@@ -8,6 +8,7 @@ import {DateTime, Badge} from '../../';
 // element's actual bounds. see DateIcon.scss for details.
 const Small = props => <div className="nti-calendar-date-icon-small-wrapper" {...props} />;
 const Fragment = ({children}) => children;
+const Badged = props => <Badge position={Badge.POSITIONS.TOP_RIGHT} {...props} {...Badge.offset(0, 4)} />;
 
 export default class DateIcon extends React.PureComponent {
 
@@ -16,22 +17,25 @@ export default class DateIcon extends React.PureComponent {
 		date: PropTypes.object,
 		badge: PropTypes.number,
 		small: PropTypes.bool,
+		minimal: PropTypes.bool,
 		viewed: PropTypes.bool
 	}
 
 	render () {
-		const {date = new Date(), small, className} = this.props;
+		const {date = new Date(), small, minimal, className, badge, children} = this.props;
 		const Wrapper = small ? Small : Fragment;
+		const Container = badge == null ? Fragment : Badged;
 
 		return (
-			<Badge position={Badge.POSITIONS.TOP_RIGHT} {...this.props} {...Badge.offset(0, 4)}>
+			<Container {...this.props}>
 				<Wrapper>
-					<div className={cx('nti-calendar-date-icon', {small}, className)}>
+					<div className={cx('nti-calendar-date-icon', {small, minimal}, className)}>
 						<div className="month">{DateTime.format(date, 'MMM')}</div>
 						<div className="day">{DateTime.format(date, 'D')}</div>
+						{children}
 					</div>
 				</Wrapper>
-			</Badge>
+			</Container>
 		);
 	}
 }
