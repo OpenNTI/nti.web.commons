3849a3c37d0eaf89cd9613cc013bfba2153453ba
Ray Hatfield <ray.hatfield@nextthought.com>
2019-04-09 17:27:38 -0500

    fix import

--- src/date-time/components/DateIcon.jsx
+++ src/date-time/components/DateIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-import {Badge} from '../../../';
+import Badge from '../../components/Badge';
 import {format} from '../utils';
 
 
