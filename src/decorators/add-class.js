4ad02c9285ad9e5a6062a07183a1efe405c56006
Andrew Ligon <andrew.ligon@nextthought.com>
2019-05-01 17:55:23 -0500

    hoist statics on the add class decorator

--- src/decorators/add-class.js
+++ src/decorators/add-class.js
@@ -1,20 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {HOC} from '@nti/lib-commons';
 import {addClass as add, removeClass as remove} from '@nti/lib-dom';
 
+class AddClassToBody extends React.Component {
+	static propTypes = {
+		_node: PropTypes.object,
+		_className: PropTypes.string,
+		_forwardRef: PropTypes.func,
+		_component: PropTypes.any
+	}
+
+	componentDidMount () {
+		const {_node, _className} = this.props;
+
+		add(_node, _className);
+	}
+
+	componentWillUnmount () {
+		const {_node, _className} = this.props;
+
+		remove(_node, _className);
+	}
+
+	render () {
+		const {_component:Component, _forwardRef: ref, ...otherProps} = this.props;
+
+		delete otherProps._node;
+		delete otherProps._className;
+
+		return (
+			<Component {...otherProps} ref={ref} />
+		);
+	}
+}
+
 export default function addClass (node, className) {
+	if (!node) { throw new Error('addClass decorator must be provided a node to add the class to.'); }
+	if (!className) { throw new Error('addClass decorator must be provided a class to add.'); }
+
 	return function factory (Component) {
-		return class AddClassToBody extends React.Component {
-			componentDidMount () {
-				add(node, className);
-			}
-
-			componentWillUnmount () {
-				remove(node, className);
-			}
-
-			render () {
-				return <Component {...this.props} />;
-			}
+		const AddClassWrapper = (props, ref) => {
+			return (
+				<AddClassToBody
+					{...props}
+					_node={node}
+					_className={className}
+					_component={Component}
+					_forwardRef={ref}
+				/>
+			);
 		};
+		const cmp = React.forwardRef(AddClassWrapper);
+
+		HOC.hoistStatics(cmp, Component, 'addClass');
+
+		return cmp;
 	};
 }
