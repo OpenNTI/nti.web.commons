e02e14a9a5c19818de63435d9bd083aaf139901b
Andrew Ligon <andrew.ligon@nextthought.com>
2019-07-01 16:56:00 -0500

    add a forward ref decorator

---
+++ src/decorators/forward-ref.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {HOC} from '@nti/lib-commons';
+
+export default function forwardRef (refProp = 'forwardedRef') {
+	return (Component) => {
+		const ForwardRefWrapper = (props, ref) => {
+			return React.createElement(Component, {...props, [refProp]: ref});
+		};
+		const cmp = React.forwardRef(ForwardRefWrapper);
+
+		const typeName = Component ? Component.displayName || Component.name : '';
+		const name = `Forwarded-Ref(${typeName})`;
+
+		HOC.hoistStatics(cmp, Component, name);
+
+		return cmp;
+	};
+}
--- src/decorators/index.js
+++ src/decorators/index.js
@@ -1,2 +1,3 @@
 export addClassToRoot from './add-class-to-root';
 export fullScreenMonitor from './full-screen-monitor';
+export ForwardRef from './forward-ref';
