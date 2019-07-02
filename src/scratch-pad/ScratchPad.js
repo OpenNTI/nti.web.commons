6be666c4b81c88d9103d78f3974284f7c0e73b2d
Andrew Ligon <andrew.ligon@nextthought.com>
2019-07-02 18:30:56 -0500

    add a utility to do dom work without displaying it

---
+++ src/scratch-pad/ScratchPad.js
@@ -0,0 +1,69 @@
+import {createDOM} from '@nti/lib-dom';
+
+const getBody = () => typeof document !== 'undefined' ? document.body : null;
+
+const containerConfig = {
+	style: {
+		position: 'absolute',
+		left: 0,
+		bottom: 0,
+		height: 0,
+		'pointer-events': 'none',
+		border: 'none',
+		overflow: 'hidden',
+		padding: '0',
+		visibility: 'hidden'
+	}
+};
+
+export default class ScratchPad {
+	static withStyles (style) {
+		return new ScratchPad({style});
+	}
+
+	static work (...args) {
+		const pad = new ScratchPad();
+
+		return pad.work(...args);
+	}
+
+	#scratchPad = null
+	#config = null
+	#container = null
+	#node = null
+
+	constructor (config = {}) {
+		this.#config = config;
+	}
+
+	#createNode = () => {
+		if (this.#node) { return this.#node; }
+
+		const body = getBody();
+
+		if (!body) { throw new Error('Cannot use scratch bad without a body'); }
+
+		this.#container = createDOM(containerConfig, body);
+		this.#node = createDOM(this.#config, this.#container);
+
+		return this.#node;
+	}
+
+	#removeNode = () => {
+		const body = getBody();
+
+		if (!body) { return; }
+		if (this.#container) { body.removeChild(this.#container); }
+	}
+
+
+	async work (fn) {
+		const node = this.#createNode();
+
+		try {
+			await fn(node);
+		} finally {
+			this.#removeNode();
+		}
+	}
+}
