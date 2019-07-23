6ecde4c596f22b31e347e3a1ccac6cf3871d8c1e
Andrew Ligon <andrew.ligon@nextthought.com>
2019-07-23 17:08:24 -0500

    add more text transform tests

---
+++ src/text/transforms/__test__/Overflow.spec.js
@@ -0,0 +1,21 @@
+/* eslint-env jest */
+import Overflow from '../Overflow';
+
+describe('Overflow transform tests', () => {
+	describe('statics', () => {
+		describe('shouldApply',  () => {
+			test('true if overflow is non null and not hasComponents', () => {
+				expect(Overflow.shouldApply({overflow: '...', hasComponents: false})).toBeTruthy();
+				expect(Overflow.shouldApply({overflow: '', hasComponents: false})).toBeTruthy();
+			});
+
+			test('false if overflow is non null and hasComponents', () => {
+				expect(Overflow.shouldApply({overflow: '...', hasComponents: true})).toBeFalsy();
+			});
+
+			test('false if overflow is null and not hasComponents', () => {
+				expect(Overflow.shouldApply({overflow: undefined, hasComponents: false})).toBeFalsy();
+			});
+		});
+	});
+});
---
+++ src/text/transforms/__test__/index.spec.js
@@ -0,0 +1,31 @@
+/* eslint-env jest */
+import {getTransforms} from '../index';
+import LimitLines from '../LimitLines';
+import Linkify from '../Linkify';
+import Overflow from '../Overflow';
+
+describe('getTransform tests', () => {
+	test('empty array if no transforms apply', () => {
+		const transforms = getTransforms({hasComponents: true});
+
+		expect(transforms.length).toBe(0);
+	});
+
+	test('returns linkify before overflow', () => {
+		const transforms = getTransforms({linkify: true, overflow: '...'});
+
+		expect(transforms.indexOf(Linkify)).toBeLessThan(transforms.indexOf(Overflow));
+	});
+
+	test('returns linkify before limit-lines', () => {
+		const transforms = getTransforms({linkify: true, limitLines: 2});
+
+		expect(transforms.indexOf(Linkify)).toBeLessThan(transforms.indexOf(LimitLines));
+	});
+
+	test('returns overflow before limit-lines', () => {
+		const transforms = getTransforms({overflow: '...', limitLines: 2});
+
+		expect(transforms.indexOf(Overflow)).toBeLessThan(transforms.indexOf(LimitLines));
+	});
+});
