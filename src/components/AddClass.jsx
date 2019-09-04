152e12941e3b4efde66eeae5fdc6edeeea054dd7
Jonathan Grimes <jsg2021@gmail.com>
2019-09-04 15:48:56 -0500

    fix unguarded document reference (blows up node when importing)

--- src/components/AddClass.jsx
+++ src/components/AddClass.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {addClass, removeClass} from '@nti/lib-dom';
 
 function add (node, className) {
@@ -13,40 +12,7 @@ function add (node, className) {
 	};
 }
 
-export default class AddClass extends React.Component {
-
-	static propTypes = {
-		node: PropTypes.object,
-		className: PropTypes.string
-	}
-
-	static defaultProps = {
-		node: document.body,
-	}
-
-	componentDidMount () {
-		this.setUp();
-	}
-	
-	setUp () {
-		const {node, className} = this.props;
-		this.remove = add(node, className);
-	}
-
-	componentDidUpdate ({node: pNode, className: pClass}) {
-		const {node, className} = this.props;
-
-		if (node !== pNode || className !== pClass) {
-			this.remove();
-			this.setUp();
-		}
-	}
-
-	componentWillUnmount () {
-		this.remove();
-	}
-
-	render () {
-		return null;
-	}
+export default function AddClass ({node = document.body, className}) {
+	React.useEffect(() => add(node, className), [node, className]);
+	return null;
 }
