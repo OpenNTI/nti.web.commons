87e55bb4661e03da27b27c621f24d605a429c0ee
Andrew Ligon <andrew.ligon@nextthought.com>
2019-09-11 14:53:11 -0500

    Allow a prop to compute the props for the child given an image src

--- src/image/Base.jsx
+++ src/image/Base.jsx
@@ -6,13 +6,15 @@ BaseImage.propTypes = {
 	alt: PropTypes.string.isRequired,
 	children: PropTypes.any,
 	childProp: PropTypes.string,
+	childProps: PropTypes.func,
 	imageRef: PropTypes.func
 };
-export default function BaseImage ({src, alt, children, childProp = 'src', imageRef, ...otherProps}) {
+export default function BaseImage ({src, alt, children, childProp = 'src', childProps, imageRef, ...otherProps}) {
 	const child = children && React.Children.only(children);
 
 	if (child) {
-		return (React.cloneElement(child, {[childProp]: src, ref: imageRef}));
+		const props = childProps ? childProps(src) : {[childProp]: src};
+		return (React.cloneElement(child, {...props, ref: imageRef}));
 	}
 
 	return (
--- test/app/Image.jsx
+++ test/app/Image.jsx
@@ -5,8 +5,21 @@ import {Image} from '../../src';
 const SRC = 'https://picsum.photos/200/300';
 const BAD_SRC = 'https://thisdoesntexistimeanhowcoulditijustmadeitup.com/image.png';
 
+function getBackground (src) {
+	return {
+		style: {
+			backgroundImage: `url(${src})`,
+			backgroundSize: 'cover',
+			width: '200px',
+			height: '200px'
+		},
+	};
+}
+
 export default function ImageTest () {
 	return (
-		<Image src={SRC} fallback={BAD_SRC} alt="Test Image" aspectRatio={Image.AspectRatios.Square} letterbox="black" />
+		<Image src={SRC} fallback={BAD_SRC} alt="Test Image" aspectRatio={Image.AspectRatios.Square} letterbox="black" childProps={getBackground}>
+			<div className="test-background-image" />
+		</Image>
 	);
 }
