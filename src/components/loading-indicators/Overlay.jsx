238b6606fdde06eb3c34fa0158bbf3b86c1a24bf
Andrew Ligon <andrew.ligon@nextthought.com>
2019-09-24 13:47:48 -0500

    Add a loading overlay component

---
+++ src/components/loading-indicators/Overlay.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+import {scoped} from '@nti/lib-locale';
+
+import {Mount} from '../../transitions';
+import Text from '../../text';
+
+import Styles from './Overlay.css';
+import Spinner from './Spinner';
+
+const cx = classnames.bind(Styles);
+const t = scoped('common.components.loading-indicators.Overlay', {
+	label: 'Loading...'
+});
+
+
+LoadingOverlay.propTypes = {
+	className: PropTypes.string,
+	loading: PropTypes.bool,
+	label: PropTypes.string,
+
+	large: PropTypes.bool
+};
+export default function LoadingOverlay ({className, loading, label = t('label'), large, ...otherProps}) {
+	return (
+		<Mount mount={loading}>
+			<div className={cx('loading-overlay', className, {large})} {...otherProps} >
+				{label && (<Text.Base className={cx('message')}>{label}</Text.Base>)}
+				{!large && (<Spinner />)}
+				{large && (<Spinner.Large />)}
+			</div>
+		</Mount>
+	);
+}
--- src/components/loading-indicators/index.js
+++ src/components/loading-indicators/index.js
@@ -1,6 +1,7 @@
 export Ellipse from './Ellipsis'; // deprecated
 export Ellipsis from './Ellipsis';
 export Mask from './Mask';
+export Overlay from './Overlay';
 export Placeholder from './Placeholder';
 export Spinner from './Spinner';
 export Whacky from './Whacky';
