4e7279f397cd244971954f7c2117ffca4a9365d9
Ray Hatfield <ray.hatfield@gmail.com>
2019-10-02 17:32:11 -0500

    first pass on a general-purpose tabs component

---
+++ src/tabs/Tab.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+
+import styles from './Tab.css';
+
+const cx = classnames.bind(styles);
+
+
+export default function Tab ({label, active, className}) {
+	return (
+		<div className={cx('nti-tab', { active }, className)}>{label}</div>
+	);
+}
+
+Tab.propTypes = {
+	label: PropTypes.node,
+	active: PropTypes.bool,
+};
---
+++ src/tabs/Tabs.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+
+import Styles from './Tabs.css';
+
+const cx = classnames.bind(Styles);
+
+
+export default function Tabs ({children, active: a = 0, onChange}) {
+	return (
+		<ul className={cx('nti-tabs')}>
+			{
+				React.Children.map(children,
+					(c, i) => {
+						const active = a === i;
+						return (
+							<li key={i} className={cx('test', { active })} onClick={() => onChange(i)}>
+								{
+									active
+										? React.cloneElement(c, { active: true })
+										: c
+								}
+							</li>
+						);
+					}
+				)
+			}
+		</ul>
+	);
+}
+
+Tabs.propTypes = {
+	active: PropTypes.number,
+	onChange: PropTypes.func.isRequired,
+};
