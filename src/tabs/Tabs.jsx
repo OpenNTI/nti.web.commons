17f8cd19e8f24978ff5354f74f9c5dca8d8104e4
Andrew Ligon <andrew.ligon@nextthought.com>
2019-10-08 16:13:07 -0500

    NTI-8584 add a classname prop to tabs

--- src/tabs/Tabs.jsx
+++ src/tabs/Tabs.jsx
@@ -7,9 +7,9 @@ import Styles from './Tabs.css';
 const cx = classnames.bind(Styles);
 
 
-export default function Tabs ({children, active: a = 0, onChange}) {
+export default function Tabs ({children, className, active: a = 0, onChange}) {
 	return (
-		<ul className={cx('nti-tabs')}>
+		<ul className={cx('nti-tabs', className)}>
 			{
 				React.Children.map(children,
 					(c, i) => {
@@ -33,4 +33,5 @@ export default function Tabs ({children, active: a = 0, onChange}) {
 Tabs.propTypes = {
 	active: PropTypes.number,
 	onChange: PropTypes.func.isRequired,
+	className: PropTypes.string
 };
