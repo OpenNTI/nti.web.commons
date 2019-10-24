3457fd5d9b9c8c96da3b4a2feb7577e44d1ff7f8
Andrew Ligon <andrew.ligon@nextthought.com>
2019-10-24 14:42:44 -0500

    expose a global theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,10 +1,22 @@
 import Context from './Context';
 import * as Hook from './Hook';
+import themeBuilder from './build-theme';
 
-export buildTheme from './build-theme';
+export const buildTheme = themeBuilder;
 export const Consumer = Context.Consumer;
 export const useTheme = Hook.useTheme;
 export const useThemeProperty = Hook.useThemeProperty;
 
 export Apply from './Apply';
 export Scope from './Scope';
+
+let globalTheme = null;
+
+export const getGlobalTheme = () => {
+	if (!globalTheme) {
+		globalTheme = buildTheme(void 0, global.themeOverride || {});
+	}
+
+
+	return globalTheme;
+};
