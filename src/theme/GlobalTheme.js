f7fcf6139c06bb84697fec7b0fa8033efbfbc5ee
Andrew Ligon <andrew.ligon@nextthought.com>
2019-10-30 15:04:58 -0500

    add utilities for applying the site brand theme

--- src/theme/GlobalTheme.js
+++ src/theme/GlobalTheme.js
@@ -4,9 +4,25 @@ let globalTheme = null;
 
 export const getGlobalTheme = () => {
 	if (!globalTheme) {
-		globalTheme = buildTheme(void 0, global.themeOverride || {});
+		globalTheme = buildTheme();
 	}
 
-
 	return globalTheme;
+};
+
+export const setGlobalThemeOverrides = (overrides) => {
+	const theme = getGlobalTheme();
+
+	theme.setOverrides(overrides);
+};
+
+//This should probably live in the apps, but we need it in both web and mobile.
+//This is just a convenient place to stick it for now
+export const siteBrandToTheme = (siteBrand) => {
+	return {
+		...(siteBrand.theme || {}),
+		brandName: siteBrand['brand_name'],
+		brandColor: siteBrand['brand_color'],
+		assets: siteBrand.assets
+	};
 };
