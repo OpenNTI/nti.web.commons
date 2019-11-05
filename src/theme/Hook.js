79054b21729563ce4ca0a6e24f208c39d043dcf2
Andrew Ligon <andrew.ligon@nextthought.com>
2019-11-05 17:20:19 -0600

    update the lib.commons ObjectUtils import

--- src/theme/Hook.js
+++ src/theme/Hook.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import {get} from '@nti/lib-commons';
+import {ObjectUtils} from '@nti/lib-commons';
 
 import ThemeContext from './Context';
 
@@ -12,5 +12,5 @@ export function useTheme () {
 export function useThemeProperty (prop) {
 	const theme = useTheme();
 
-	return theme && get(theme, prop);
+	return theme && ObjectUtils.get(theme, prop);
 }
