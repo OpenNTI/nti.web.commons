7c2895cd6419219864b16d4ddf43221f40abdb4d
Andrew Ligon <andrew.ligon@nextthought.com>
2019-11-07 18:52:19 -0600

    add better default assets

--- src/theme/fallback-assets/index.js
+++ src/theme/fallback-assets/index.js
@@ -1,10 +1,11 @@
-import Logo from './logo.png';
+import Logo from './logo@3.png';
+import FullLogo from './full_logo@3.png';
 import Email from './email_logo.png';
 import Favicon from './favicon.ico';
 
 export default {
 	Logo,
-	FullLogo: Logo,
+	FullLogo,
 	Email,
 	Favicon
 };
