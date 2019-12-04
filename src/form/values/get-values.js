2d9fb71a83dcc9eedd4e34ea86256f517a39be8f
Andrew Ligon <andrew.ligon@nextthought.com>
2019-12-04 11:45:19 -0600

    NTI-8746 pass validation errors along with the values

--- src/form/values/get-values.js
+++ src/form/values/get-values.js
@@ -1,9 +1,14 @@
+import {getValidationErrors, isValid} from '../validation';
+
 import getFormData from './get-form-data';
 import getJSON from './get-json';
 
 export default function getValues (form) {
 	return {
 		get formData () { return getFormData(form); },
-		get json () { return getJSON(form); }
+		get json () { return getJSON(form); },
+		get isValid () { return isValid(form); },
+
+		getValidationErrors: () => getValidationErrors(form)
 	};
 }
