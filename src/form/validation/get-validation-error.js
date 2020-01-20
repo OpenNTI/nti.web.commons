01d4054db6892b594d21ddae1ab730480e918e8e
Jonathan Troyer <jonathan.troyer@nextthought.com>
2020-01-20 12:54:11 -0600

    Add validation message customizatiom. Also add a validation message for patternMissmatch

--- src/form/validation/get-validation-error.js
+++ src/form/validation/get-validation-error.js
@@ -2,16 +2,21 @@ import {scoped} from '@nti/lib-locale';
 
 const t = scoped('nti-web-commons.form.validation.get-validation-error', {
 	missing: 'Please fill out this field.',
-	invalidEmail: 'Please provide a valid email.'
+	invalidEmail: 'Please provide a valid email.',
+	patternMismatch: 'Please match the requested format'
 });
 
 const checks = [
-	{message: t('missing'), check: field => field.required && field.validity.valueMissing},
-	{message: t('invalidEmail'), check: field => field.type === 'email' && field.validity.typeMismatch}
+	{message: (field) => field.dataset.missingMessage ?? t('missing'), check: field => field.required && field.validity.valueMissing},
+	{message: (field) => field.dataset.invalidEmailMessage ?? t('invalidEmail'), check: field => field.type === 'email' && field.validity.typeMismatch},
+	{message: (field) => field.dataset.patternMismatchMessage ?? t('patternMismatch'), check: field => field.validity.patternMismatch}
 ];
 
 export default function getValidationError (field) {
 	for (let check of checks) {
-		if (check.check(field)) { return check.message; }
+		if (check.check(field)) {
+			return check.message(field);
+		}
 	}
+	return field.validationMessage;
 }
