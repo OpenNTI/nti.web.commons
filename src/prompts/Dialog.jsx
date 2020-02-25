22e335f01448546d263e04669e1accae50b757ec
Jonathan Grimes <jsg2021@gmail.com>
2020-02-25 15:09:11 -0600

    Update uuid

--- src/prompts/Dialog.jsx
+++ src/prompts/Dialog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import Logger from '@nti/util-logger';
-import uuid from 'uuid/v4';
+import uuid from 'uuid';
 
 import Manager from './ModalManager';
 
@@ -55,7 +55,7 @@ export default class Dialog extends React.Component {
 
 
 	componentDidMount () {
-		this.uuid = uuid();
+		this.uuid = uuid.v4();
 		this.rollbackCountStateKey = this.uuid + '-rollbackCount';
 
 		global.addEventListener('popstate', this.onHistory);
