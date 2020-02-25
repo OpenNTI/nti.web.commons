7da67e0abda5706214b004bb1718bc7f7770823f
Andrew Ligon <andrew.ligon@nextthought.com>
2020-02-24 18:28:13 -0600

    checkpoint for the error state

--- src/components/inputs/LabelPlaceholder.jsx
+++ src/components/inputs/LabelPlaceholder.jsx
@@ -47,7 +47,7 @@ export default function LabelPlaceholder ({className, style = Box, as:tag, label
 			{label && (
 				<Text.Base as="label" className={cx('label-placeholder')} htmlFor={id}>{label}</Text.Base>
 			)}
-			<ErrorMessage error={error} className={cx('error-message')} id={errorId} role="alert" />
+			{error && (<ErrorMessage error={error} className={cx('error-message')} id={errorId} role="alert" />)}
 		</Cmp>
 	);
 }
