77527811327dd0ae794a5ded149ba3424f89d4be
Andrew Ligon <andrew.ligon@nextthought.com>
2020-04-08 12:51:10 -0500

    NTI-9128 don't trigger offline if the page is unloading

--- src/offline/Hooks.js
+++ src/offline/Hooks.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import {getServer} from '@nti/web-client';
 
+let unloading = false;
+const setUnloading = () => unloading = true;
+
 export function useOffline () {
 	const server = React.useRef(getServer());
 	const [offline, setOffline] = React.useState(false);
 
 	React.useEffect(() => {
-		return server.current.OnlineStatus.subscribeToChange(online => setOffline(!online));
+		window.addEventListener('beforeunload', setUnloading);
+
+		return () => {
+			window.removeEventListener('beforeunload', setUnloading);
+		};
+	}, []);
+
+	React.useEffect(() => {
+		return server.current.OnlineStatus.subscribeToChange(online => {
+			if (!unloading) {
+				setOffline(!online);
+			}
+		});
 	}, []);
 
 	return offline;
