892ee11d9fe375c1248e8d73623e05f46ad50218
Andrew Ligon <andrew.ligon@nextthought.com>
2020-06-04 18:25:00 -0500

    add more icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,3 +1,7 @@
 //TODO: export the icon font icons from here too
 
 export Download from './Download';
+export Drawing from './Drawing';
+export FileUpload from './FileUpload';
+export Image from './Image';
+export Video from './Video';
