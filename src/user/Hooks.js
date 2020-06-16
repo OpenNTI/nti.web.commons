fd30a93ae4039e1eb65c9b88794c6e8327fe55c4
Andrew Ligon <andrew.ligon@nextthought.com>
2020-06-16 12:31:33 -0500

    add a hook to resolve a user

---
+++ src/user/Hooks.js
@@ -0,0 +1,34 @@
+import {User, getAppUsername} from '@nti/web-client';
+import {scoped} from '@nti/lib-locale';
+
+import {useResolver} from '../hooks';
+
+const {isResolved} = useResolver;
+const t = scoped('web-commons.user.Hooks', {
+	anonymous: 'Anonymous User'
+});
+
+const getId = x => x?.getID?.() ?? x;
+const getUnresolved = () => t('anonymous'); // do we need to show identifying number
+
+export function useUser (user) {
+	const resolver = useResolver(async () => {
+		try {
+			const id = !user || user === 'me' ? getAppUsername() : getId(user);
+			const entity = await User.resolve({entity: id});
+
+			return entity;
+		} catch (e) {
+			const unresolved = getUnresolved();
+
+			return {
+				displayName: unresolved,
+				username: unresolved,
+				getID: () => '__unresolvedUser__',
+				anonymous: true
+			};
+		}
+	}, [user]);
+
+	return isResolved(resolver) ? resolver : null; 
+}
--- src/user/index.js
+++ src/user/index.js
@@ -1,3 +1,4 @@
 export Avatar from './Avatar';
 export DisplayName from './DisplayName';
 export Presence from './Presence';
+export * from './Hooks';
