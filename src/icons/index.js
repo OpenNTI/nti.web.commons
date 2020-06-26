9a958dad33806147803431e014e080896a5b1bce
Andrew Ligon <andrew.ligon@nextthought.com>
2020-06-26 00:15:21 -0500

    add a person icon

--- src/icons/index.js
+++ src/icons/index.js
@@ -7,6 +7,7 @@ export FontType from './FontType';
 export Image from './Image';
 export Lock from './Lock';
 export PaperClip from './PaperClip';
+export Person from './Person';
 export Share from './Share';
 export Video from './Video';
 export X from './X';
