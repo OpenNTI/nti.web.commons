fa7d3855bb20e3362874bbab31f01ce0905bcdf2
Andrew Ligon <andrew.ligon@nextthought.com>
2020-10-06 14:36:35 -0500

    Add a task button component to have an indicator while the button's action is running

---
+++ src/task/components/Button.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+
+import {Button, Loading} from '../../components';
+import Text from '../../text';
+
+import Styles from './Button.css';
+
+const cx = classnames.bind(Styles);
+
+TaskButton.propTypes = {
+	className: PropTypes.string,
+	running: PropTypes.bool,
+	children: PropTypes.any
+};
+export default function TaskButton ({className, running, children, ...otherProps}) {
+	return (
+		<Button className={cx('task-button', className, {running})} {...otherProps} >
+			<Text.Base className={cx('label')}>{children}</Text.Base>
+			{running && (<Loading.Spinner white className={cx('spinner')} />)}
+		</Button>
+	);
+}
