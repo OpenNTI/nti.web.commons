4cd8fef7d3a168d9d55de30207688ea453a0efff
Andrew Ligon <andrew.ligon@nextthought.com>
2020-10-13 11:15:01 -0500

    add report icon

---
+++ src/icons/Report.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import FontIcon from './Font-Icon';
+
+const Icon = 'icon-report';
+
+export default function GearIcon (props) {
+	return (
+		<FontIcon icon={Icon} {...props} />
+	);
+}
