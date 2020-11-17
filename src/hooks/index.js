ce2967f4694a95c865fd4c18b899b3750d592524
Andrew Ligon <andrew.ligon@nextthought.com>
2020-11-17 16:49:38 -0600

    add a usePersitentState hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,5 +2,6 @@ export useChanges from './use-changes';
 export useForceUpdate from './use-force-update';
 export useMatchesMediaQuery from './use-matches-media-query';
 export useMutationObserver from './use-mutation-observer';
+export usePersistentState from './use-persistent-state';
 export useResolver from './use-resolver';
 export * from './use-responsive-value';
