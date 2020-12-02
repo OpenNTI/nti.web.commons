dba9c3f669c6e691f170722fd308f81d08159175
Andrew Ligon <andrew.ligon@nextthought.com>
2020-12-02 16:14:07 -0600

    add rounded alert icon

--- src/icons/Alert.jsx
+++ src/icons/Alert.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 
 import FontIcon from './Font-Icon';
+import SVGIcon from './SVG-Icon';
+
+AlertIcon.Round = (props) => {
+	return (
+		<SVGIcon
+			{...props}
+			viewBox="0 0 40 40"
+		>
+			<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+				<g transform="translate(-411.000000, -187.000000)">
+					<g transform="translate(411.000000, 187.000000)">
+						<circle stroke="currentColor" strokeWidth="3" cx="20" cy="20" r="18.5"></circle>
+						<text fontFamily="OpenSans-Bold, Open Sans" fontSize="25" fontWeight="bold" fill="currentColor">
+							<tspan x="16.4233398" y="29">!</tspan>
+						</text>
+					</g>
+				</g>
+			</g>
+		</SVGIcon>
+	);
+};
+
+AlertIcon.Round.displayName = 'AlertIconRound';
 
 export default function AlertIcon (props) {
 	return (
