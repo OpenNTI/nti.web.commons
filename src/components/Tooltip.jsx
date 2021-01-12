d6a2de10ba073d660bc375f060e31da907cc2189
Mahmoud Mousa Hamad <34195384+MahmoudMousaHamad@users.noreply.github.com>
2021-01-12 20:50:27 +0300

    Add tooltip component (#154)
    * Add tooltip component
    
    * Add Tooltip to exports
    
    * Make tooltip prop be anything
    
    * Check if tooltip is a string and wrap in Text
    
    * Use Reach and add styles based on existing ones.
    
    * Use shorter way of implementing the component
    
    * Add Reach tooltip to package.json
    
    * Don't use internal index imports

---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tooltip from '@reach/tooltip';
+
+import {Text} from '.../text/index.js';
+
+export default styled(Tooltip).attrs(({label, ...props}) => ({
+	...props,
+	label: typeof label === 'string' ? <Text>{label}</Text> : label,
+})
+)`
+		background-color: var(--primary-grey);
+		padding: 3px 5px;
+		box-shadow: 1px 2px 5px 0 rgba(0, 0, 0, 0.25);
+		text-transform: uppercase;
+		color: #FFFFFF;
+		font-size: 11px;
+		font-weight: 600;
+`;
