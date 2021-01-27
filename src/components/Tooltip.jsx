b9baedd277b3c9cb9cdbcd782b78846d0775d653
Jonathan Grimes <jsg2021@gmail.com>
2021-01-27 16:39:30 -0600

    lint

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -12,7 +12,7 @@ export default styled(Tooltip).attrs(({label, ...props}) => ({
 		padding: 3px 5px;
 		box-shadow: 1px 2px 5px 0 rgba(0, 0, 0, 0.25);
 		text-transform: uppercase;
-		color: #FFFFFF;
+		color: #fff;
 		font-size: 11px;
 		font-weight: 600;
 		width: fit-content;
