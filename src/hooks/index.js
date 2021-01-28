2235152973a1b64ff8b830e253701c642ab50364
Jonathan Grimes <jsg2021@gmail.com>
2021-01-28 11:23:09 -0600

    add useWindowSize

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -4,4 +4,5 @@ export {useMediaQuery} from './use-media-query';
 export {useMutationObserver} from './use-mutation-observer';
 export {usePersistentState} from './use-persistent-state';
 export {useResolver} from './use-resolver';
+export {useWindowSize} from './use-window-size';
 export * from './use-responsive-value';
