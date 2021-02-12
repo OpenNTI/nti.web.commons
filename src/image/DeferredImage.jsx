30dd445c0bab0d1dffa579f961cbfd9046524082
Ray Hatfield <ray.hatfield@gmail.com>
2021-02-12 16:54:48 -0600

    add loading shimmer animation to DeferredImage

--- src/image/DeferredImage.jsx
+++ src/image/DeferredImage.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 import { OnScreen } from '../components/monitors';
 import { DataURIs } from '../constants';
 
 const { BLANK_IMAGE } = DataURIs;
 
+const styles = css`
+	.loading {
+		animation: shimmer 2s infinite;
+		background: linear-gradient(to right, #eff1f3 4%, #e2e2e2 25%, #eff1f3 36%);
+		background-size: 1000px 100%;
+	}
+	@keyframes shimmer {
+		0% { background-position: -1000px 0; }
+		100% { background-position: 1000px 0; }
+	}
+`;
+
 export default function DeferredImage ({src, className, ...props}) {
-	const [source, setSource] = React.useState(BLANK_IMAGE);
-	const onChange = React.useCallback((onScreen) => {
+	const [source, setSource] = useState(BLANK_IMAGE);
+	const [loaded, setLoaded] = useState(false);
+	const onLoad = useCallback(() => {
+		if (!loaded && src === source) {
+			setLoaded(true);
+		}
+	}, [src, source]);
+
+	const onChange = useCallback((onScreen) => {
 		if (onScreen && (src !== source)) {
 			setSource(src);
 		}
 	}, [src]);
 
 	return (
-		<OnScreen onChange={onChange} as="img" src={source} {...props} />
+		<OnScreen
+			onChange={onChange}
+			onLoad={onLoad}
+			as="img"
+			src={source}
+			className={cx({[styles.loading]: !loaded}, className)}
+			{...props}
+		/>
 	);
 }
 
