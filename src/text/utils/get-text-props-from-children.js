30dd2a7fa3dfa2ff7cb622bcd9ca874819c9c552
Jonathan Grimes <jsg2021@gmail.com>
2021-02-12 13:17:00 -0600

    [NTI-10258] prevent getTextPropsFromChildren from throwing

--- src/text/utils/get-text-props-from-children.js
+++ src/text/utils/get-text-props-from-children.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import {MARKUP_WHITE_LIST} from '../constants';
+import { MARKUP_WHITE_LIST } from '../constants';
 
 const HasAnchorRegex = /<\/?a [^>]*>/i;
 const HasNonAnchorMarkup = /<\/?[^a]>/i;
 
-function hasAllowedRawMarkup (text) {
+function hasAllowedRawMarkup(text) {
 	return HasAnchorRegex.test(text) && !HasNonAnchorMarkup.test(text);
 }
 
-function getAttributesFromProps (props) {
+function getAttributesFromProps(props) {
 	const names = Object.keys(props);
 	let attributes = '';
 
@@ -24,23 +24,32 @@ function getAttributesFromProps (props) {
 	return attributes;
 }
 
-function getMarkupFromChild (child) {
-	const {type, props} = child;
-	const {children, ...attrProps} = props || {};
+function getMarkupFromChild(child) {
+	const { type, props } = child;
+	const { children, ...attrProps } = props || {};
 
 	const tag = type;
 	const attributes = getAttributesFromProps(attrProps);
 	const childProps = children ? getTextPropsFromChildren(children) : {};
 
-	if (childProps.hasComponents) { throw new Error('Cannot get markup from components'); }
+	if (childProps.hasComponents) {
+		throw new Error('Cannot get markup from components');
+	}
 
 	const innerHTML = childProps.text ? childProps.text : '';
 
 	return `<${tag}${attributes}>${innerHTML}</${tag}>`;
 }
 
-export default function getTextPropsFromChildren (childrenProp, allowAllMarkup) {
-	const children = React.Children.toArray(childrenProp);
+export default function getTextPropsFromChildren(childrenProp, allowAllMarkup) {
+	let children;
+	try {
+		children = React.Children.toArray(childrenProp);
+	} catch {
+		// eslint-disable-next-line no-console
+		console.warn('Unexpected child value: %o', childrenProp);
+		children = [];
+	}
 
 	let text = '';
 	let hasMarkup = allowAllMarkup;
@@ -50,7 +59,10 @@ export default function getTextPropsFromChildren (childrenProp, allowAllMarkup) 
 		if (typeof child === 'string') {
 			text += child;
 			hasMarkup = hasMarkup || hasAllowedRawMarkup(child);
-		} else if (typeof child.type === 'string' && MARKUP_WHITE_LIST.TAGS[child.type]) {
+		} else if (
+			typeof child.type === 'string' &&
+			MARKUP_WHITE_LIST.TAGS[child.type]
+		) {
 			try {
 				text += getMarkupFromChild(child);
 				hasMarkup = true;
@@ -67,5 +79,5 @@ export default function getTextPropsFromChildren (childrenProp, allowAllMarkup) 
 		}
 	}
 
-	return {text, hasMarkup, hasComponents};
+	return { text, hasMarkup, hasComponents };
 }
