52b03edb65f1cc02778c3c24d906e2197014d67e
Ray Hatfield <ray.hatfield@gmail.com>
2021-02-12 18:47:28 -0600

    preload images before setting src

--- src/image/DeferredImage.jsx
+++ src/image/DeferredImage.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
@@ -8,39 +8,63 @@ import { DataURIs } from '../constants';
 const { BLANK_IMAGE } = DataURIs;
 
 const styles = css`
-	.loading {
+	.img.loading {
 		animation: shimmer 2s infinite;
 		background: linear-gradient(to right, #eff1f3 4%, #e2e2e2 25%, #eff1f3 36%);
 		background-size: 1000px 100%;
+		background-attachment: fixed;
+	}
+	.img:not(.loading) {
+		animation: fadeup 0.5s;
+		animation-iteration-count: 1;
+		animation-fill-mode: both;
 	}
 	@keyframes shimmer {
 		0% { background-position: -1000px 0; }
 		100% { background-position: 1000px 0; }
+	}
+	@keyframes fadeup {
+		0% {
+			opacity: 0;
+			transform: scale(0.875);
+		}
+		30% {
+			transform: scale(1);
+		}
+		100% {
+			opacity: 1;
+		}
 	}
 `;
 
 export default function DeferredImage ({src, className, ...props}) {
 	const [source, setSource] = useState(BLANK_IMAGE);
 	const [loaded, setLoaded] = useState(false);
-	const onLoad = useCallback(() => {
-		if (!loaded && src === source) {
-			setLoaded(true);
-		}
-	}, [src, source]);
+
+	const onPreload = useCallback(() => {
+		setLoaded(true);
+		setSource(src);
+	});
+
+	const preloader = useRef();
 
 	const onChange = useCallback((onScreen) => {
 		if (onScreen && (src !== source)) {
-			setSource(src);
+			if (preloader.current) {
+				preloader.current.removeEventListener('load', onPreload);
+			}
+			preloader.current = new Image();
+			preloader.current.addEventListener('load', onPreload);
+			preloader.current.src = src;
 		}
 	}, [src]);
 
 	return (
 		<OnScreen
 			onChange={onChange}
-			onLoad={onLoad}
 			as="img"
 			src={source}
-			className={cx({[styles.loading]: !loaded}, className)}
+			className={cx(styles.img, {[styles.loading]: !loaded}, className)}
 			{...props}
 		/>
 	);
