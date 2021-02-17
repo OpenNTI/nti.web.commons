d402fa2532bc1c31e230e7b2d649659cef977fed
Jonathan Grimes <jsg2021@gmail.com>
2021-02-16 21:24:47 -0600

    Why won't this show up in webapp?!

--- src/image/DeferredImage.jsx
+++ src/image/DeferredImage.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useReducer} from 'react';
+import React, { useCallback, useEffect, useRef, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
@@ -12,13 +12,12 @@ const DELAY = 250;
 const styles = css`
 	.img.loading {
 		animation: shimmer 2s infinite;
-		background:
-			linear-gradient(
-				to right,
-				#eff1f3 4%,
-				#e2e2e2 25%,
-				#eff1f3 36%
-			);
+		background: linear-gradient(
+			105deg,
+			var(--panel-background-alt, #e2e2e2) 40%,
+			white 50%,
+			var(--panel-background-alt, #e2e2e2) 60%
+		);
 		background-size: 1000px 100%;
 		background-attachment: fixed;
 	}
@@ -55,58 +54,51 @@ const styles = css`
 	}
 `;
 
-export default function DeferredImage ({src, className, ...props}) {
-	const [{source, loaded, isOnScreen}, dispatch] = useReducer((state, action) => ({
-		...state,
-		...action
-	}), { source: BLANK_IMAGE, loaded: false, isOnScreen: false });
+export default function DeferredImage({ src, className, onError, ...props }) {
+	const deps = [src, onError];
+	const [{ source, loaded, onScreen }, dispatch] = useReducer(
+		(state, action) => ({
+			...state,
+			...action,
+		}),
+		{ source: BLANK_IMAGE, loaded: false, isOnScreen: false }
+	);
 
-	const onPreload = useCallback(() => {
-		dispatch({ loaded: true, source: src });
-	}, [src]);
+	console.log(src);
 
-	const preloader = useRef({ image: null });
+	const { current: internal } = useRef({ image: null });
 
-	const onError = useCallback((...args) => void props.onError?.(...args), [props.onError]);
-
-	const preload = useCallback(() => {
-		// abort any pending preload timeout
-		clearTimeout(preloader.current.timeout);
-
-		// setTimeout to defer loading so we're not kicking off requests for images that
-		// are rapidly scrolling by; quickly coming into view and going back out again.
-		preloader.current.timeout = setTimeout(() => {
-			preloader.current.image?.removeEventListener?.('load', onPreload);
-			preloader.current.image?.removeEventListener?.('error', onError);
-			preloader.current.image = new Image();
-			preloader.current.image.addEventListener('load', onPreload);
-			preloader.current.image.addEventListener('error', onError);
-			preloader.current.image.src = src;
-			delete preloader.current.timeout;
-		}, DELAY);
-	}, [src]);
-
-	// if we're already on screen when src changes, kick off the preload
 	useEffect(() => {
-		if (isOnScreen && source !== src && preloader.current.image?.src !== src) {
-			preload();
-		}
-	}, [src, source, preload]);
+		// abort any pending preload timeout
+		clearTimeout(internal.timeout);
+		const onLoad = () => dispatch({ loaded: true, source: src });
+		const _onError = (...args) => void onError?.(...args);
 
-	const onChange = useCallback((onScreen) => {
-		dispatch({ isOnScreen: onScreen });
+		const image = new Image();
+		image.addEventListener('load', onLoad);
+		image.addEventListener('error', _onError);
 
-		// if not on screen anymore kill the preload timeout
-		if (!onScreen) {
-			clearTimeout(preloader.current.timeout);
-			delete preloader.current.timeout;
+		internal.invalidate?.();
+		internal.invalidate = () => {
+			image.removeEventListener('load', onLoad);
+			image.removeEventListener('error', onError);
+		};
+
+		// if we're already on screen when src changes, kick off the load
+		if (onScreen) {
+			// setTimeout to defer loading so we're not kicking off requests for images that
+			// are rapidly scrolling by; quickly coming into view and going back out again.
+			internal.timeout = setTimeout(() => {
+				delete internal.timeout;
+				internal.image = image;
+				internal.image.src = src;
+			}, DELAY);
 		}
 
-		// if onscreen setTimeout for preloading
-		if (onScreen && (src !== source) && !preloader.current.timeout) {
-			preload();
-		}
-	}, [src, props.onError]);
+		return internal.invalidate;
+	}, [...deps, onScreen]);
+
+	const onChange = useCallback(x => dispatch({ onScreen: x }), [...deps]);
 
 	return (
 		<OnScreen
@@ -121,5 +113,5 @@ export default function DeferredImage ({src, className, ...props}) {
 
 DeferredImage.propTypes = {
 	src: PropTypes.string,
-	onError: PropTypes.func
+	onError: PropTypes.func,
 };
