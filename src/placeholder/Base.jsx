98495fdd5b5d5c128159538c4ac0a7d23bea9269
Andrew Ligon <andrew.ligon@nextthought.com>
2021-02-16 22:38:09 -0600

    checkpoint for placeholder component building blocks

---
+++ src/placeholder/Base.jsx
@@ -0,0 +1,28 @@
+export default styled.div`
+	.flat {
+		background-color: #e2e2e2;
+	}
+
+	:not(.flat) {
+		animation: shimmer 2s infinite;
+		background:
+			linear-gradient(
+				to right,
+				#eff1f3 4%,
+				#e2e2e2 25%,
+				#eff1f3 36%
+			);
+		background-size: 1000px 100%;
+		background-attachment: fixed;
+	}
+
+	@keyframes shimmer {
+		0% {
+			background-position: -1000px 0;
+		}
+
+		100% {
+			background-position: 1000px 0;
+		}
+	}
+`;
