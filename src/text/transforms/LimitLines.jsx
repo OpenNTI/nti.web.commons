0f2180fe4d9003bb633d39618b8de108429547bc
Jonathan Grimes <jsg2021@gmail.com>
2021-02-26 11:07:00 -0600

    use css to limit lines. Make forceLines a css concern for users

--- src/text/transforms/LimitLines.jsx
+++ src/text/transforms/LimitLines.jsx
@@ -1,122 +1,43 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-import { getStyles, updateRef } from '../utils';
-import { ScreenSize } from '../../decorators';
-
 const styles = stylesheet`
 	.limit-lines {
-		display: inline-block;
+		display: -webkit-box;
+		-webkit-line-clamp: var(--line-limit, 2);
+		-webkit-box-orient: vertical;
 		overflow: hidden;
+		text-overflow: ellipsis;
+
+		/* The eventual standard:
+		https://drafts.csswg.org/css-overflow-3/#propdef-line-clamp */
+		line-clamp: var(--line-limit, 2) "…";
 	}
 `;
 
-function getLinesInfo(limitLines, forceLines) {
-	if (limitLines != null) {
-		return {
-			lines: limitLines,
-			style: 'maxHeight',
-		};
-	}
-
-	if (forceLines != null) {
-		return {
-			lines: forceLines,
-			style: 'height',
-		};
-	}
-}
-
-//TODO: recompute ellipse after a resize or size change
-
 const LimitLines = React.forwardRef(
-	(
-		{
-			children,
-			style: propStyle,
-			className,
-			limitLines,
-			forceLines,
-			screenWidth,
-			screenHeight,
-			...otherProps
-		},
-		ref
-	) => {
-		const single = limitLines != null ? limitLines === 1 : forceLines === 1;
-		const textNode = useRef();
-		const [{ style: stateStyle, settled }, setState] = useState({
-			style: {},
-			settled: false,
-		});
-
-		const processRef = useCallback(
-			node => {
-				textNode.current = node;
-
-				//NOTE: We are waiting to call the textRef back until after we've set and rendered the maxHeight,
-				//but we don't need to wait when we are unmounting.
-				updateRef(ref, node);
-			},
-			[ref]
-		);
-
-		useEffect(() => {
-			const { current: node } = textNode;
-			if (!node) {
-				return;
-			}
-
-			const { lines, style } = getLinesInfo(limitLines, forceLines);
-			const { lineHeight, paddingTop, paddingBottom } = getStyles(node, [
-				'lineHeight',
-				'paddingTop',
-				'paddingBottom',
-			]);
-
-			const max =
-				Math.ceil(lineHeight * lines) +
-				(paddingTop || 0) +
-				(paddingBottom + 0);
-
-			setState({
-				style: {
-					[style]: `${max}px`,
-				},
-				settled: true,
-			});
-		}, [limitLines, forceLines, textNode.current, screenWidth]);
-
-		useEffect(() => {
-			updateRef(ref, textNode.current);
-		}, [textNode.current, stateStyle, settled]);
-
+	({ children, style, className, limitLines: limit, ...otherProps }, ref) => {
 		const Text = React.Children.only(children);
 
 		return React.cloneElement(Text, {
 			...otherProps,
-			className: cx(className, styles.limitLines, { single, settled }),
-			style: { ...(propStyle || {}), ...(stateStyle || {}) },
-			ref: processRef,
+			ref,
+			className: cx(className, styles.limitLines),
+			style: {
+				...style,
+				'--line-limit': limit,
+			},
 		});
 	}
 );
 
 LimitLines.displayName = 'LimitLines';
-LimitLines.shouldApply = ({ limitLines, forceLines }) =>
-	limitLines != null || forceLines != null;
+LimitLines.shouldApply = ({ limitLines }) => limitLines != null;
 
 LimitLines.propTypes = {
-	className: PropTypes.string,
 	limitLines: PropTypes.number,
-	forceLines: PropTypes.number,
 	style: PropTypes.object,
-	children: PropTypes.any,
-
-	//from ScreenSize decorator
-	screenWidth: PropTypes.any,
-	screenHeight: PropTypes.any,
 };
 
-export default ScreenSize()(LimitLines);
+export default LimitLines;
--- src/text/transforms/index.js
+++ src/text/transforms/index.js
@@ -1,11 +1,10 @@
 import Escape from './Escape';
 import LimitLines from './LimitLines';
 import Linkify from './Linkify';
-import Overflow from './Overflow';
 import Translate from './Translate';
 
 //NOTE: the order of these transforms matters
-const Transforms = [Translate, Escape, Linkify, LimitLines, Overflow];
+const Transforms = [Translate, Escape, Linkify, LimitLines];
 
 export function getTransforms(props) {
 	return Transforms.filter(transform => transform.shouldApply?.(props));
