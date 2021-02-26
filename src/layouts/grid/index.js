66d8cb3ed5c69d185e728f4822ae47c2a37edab5
Ray Hatfield <ray.hatfield@gmail.com>
2021-02-26 14:58:25 -0600

    make grid occupy full width below 2-column threshold

--- src/layouts/grid/index.js
+++ src/layouts/grid/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { HOC } from '@nti/lib-commons';
 
+import { useMediaQuery } from '../../hooks';
+
 import Grid from './Grid';
 
-export default function gridFactory (colsize, gap) {
-	const Component = props => <Grid colsize={colsize} gap={gap} {...props} />;
-	return HOC.hoistStatics(Component, Grid)
+export default function gridFactory(columnWidth, gap = 0) {
+	const Component = props => {
+		// occupy full width if there isn't room for more than one column
+		const colsize = useMediaQuery(`(max-width: ${columnWidth * 2 + gap}px)`)
+			.matches
+			? '100%'
+			: columnWidth;
+
+		return <Grid colsize={colsize} gap={gap} {...props} />;
+	};
+	return HOC.hoistStatics(Component, Grid);
 }
