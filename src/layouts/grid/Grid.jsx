79697e984439dd96a966229f0b0ae0a9263a2fe0
Jonathan Grimes <jsg2021@gmail.com>
2021-03-01 14:00:17 -0600

    make grid support a render child recieving column count

--- src/layouts/grid/Grid.jsx
+++ src/layouts/grid/Grid.jsx
@@ -1,38 +1,88 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
 
-import styles from './Grid.css';
+import { useContainerWidth } from '../../hooks';
+
+const Grid = styled('div')`
+	display: grid;
+	grid-template-columns:
+		[full-start] repeat(auto-fill, var(--col-width, 100%))
+		[full-end];
+	grid-auto-rows: min-content;
+	gap: var(--gap, 14px);
+	margin: 0;
+	justify-content: center;
+	list-style: none;
+	padding: 0;
+
+	/* headers and directly nested containers occupy the full available width */
+	& > &,
+	&.header,
+	&.variant-single-column > * {
+		grid-column: 1 / -1;
+	}
+
+	& > * {
+		overflow: hidden;
+	}
+`;
 
 // append 'px' if it's a number otherwise leave it alone
-const numpx = num => typeof num === 'number' ? `${num}px` : num;
+const pixels = num => (typeof num === 'number' ? `${num}px` : num);
 
 // Renders a grid container that fills with as many columns as will fit, centered
-export default function Grid ({as: Cmp = 'div', colsize, gap, className, singleColumn, ...other}) {
+export default function GridLogic({
+	children,
+	colWidth: width,
+	gap,
+	singleColumn,
+	...other
+}) {
+	const ref = useRef();
+	const { current: el } = ref;
+	const containerWidth = useContainerWidth(ref);
+
+	const childRenderer = typeof children === 'function';
 	const cssProperties = {
-		'--colsize': numpx(colsize),
-		'--gap': numpx(gap),
+		'--col-width': pixels(width),
+		'--gap': pixels(gap),
 	};
+
+	const columnWidth =
+		width ||
+		(!el
+			? 0
+			: parseInt(
+					// should be reading from a cached style computation
+					getComputedStyle(el).getPropertyValue('--col-width'),
+					10
+			  ));
+
+	const columns = singleColumn
+		? 1
+		: Math.max(1, Math.floor(containerWidth / columnWidth) || 1);
+
 	return (
-		<Cmp
+		<Grid
+			ref={ref}
 			style={cssProperties}
-			className={cx(styles.container, {[styles.singleColumn]: singleColumn}, className)}
+			variant={singleColumn ? 'single-column' : null}
 			{...other}
-		/>
+		>
+			{childRenderer ? children(columns) : children}
+		</Grid>
 	);
 }
 
 // sets 'grid-column: 1 / -1' to occupy a full row
-Grid.Header = ({as: Cmp = 'div', className, ...props}) => (
-	<Cmp className={cx(styles.header, className)} {...props} />
-);
+GridLogic.Header = props => <GridLogic header {...props} />;
 
-Grid.propTypes = {
-	as: PropTypes.node,
-	colsize: PropTypes.oneOfType([
+GridLogic.propTypes = {
+	as: PropTypes.any,
+	colWidth: PropTypes.oneOfType([
 		PropTypes.number, // e.g. 200
-		PropTypes.string // e.g. 'minmax(200, 1fr)'
+		PropTypes.string, // e.g. 'minmax(200, 1fr)'
 	]),
 	gap: PropTypes.number,
-	singleColumn: PropTypes.bool // provides a "block" element while retaining the grid's width-snapping characteristics
+	singleColumn: PropTypes.bool, // provides a "block" element while retaining the grid's width-snapping characteristics
 };
--- src/layouts/grid/index.js
+++ src/layouts/grid/index.js
@@ -8,12 +8,13 @@ import Grid from './Grid';
 export default function gridFactory(columnWidth, gap = 0) {
 	const Component = props => {
 		// occupy full width if there isn't room for more than one column
-		const colsize = useMediaQuery(`(max-width: ${columnWidth * 2 + gap}px)`)
-			.matches
+		const colWidth = useMediaQuery(
+			`(max-width: ${columnWidth * 2 + gap}px)`
+		).matches
 			? '100%'
 			: columnWidth;
 
-		return <Grid colsize={colsize} gap={gap} {...props} />;
+		return <Grid colWidth={colWidth} gap={gap} {...props} />;
 	};
 	return HOC.hoistStatics(Component, Grid);
 }
