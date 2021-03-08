64cf0f09576dc06674458a984ed87e6736dcf8c1
Ray Hatfield <ray.hatfield@gmail.com>
2021-03-08 14:51:44 -0600

    incremental work on card styling

--- src/layouts/card-list/Card.jsx
+++ src/layouts/card-list/Card.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import cx from 'classnames';
 
 import { Card as StandardCard } from '../../standard-ui';
 import Image from '../../image';
 import { Asset } from '../../presentation-assets';
+import { useListItemVariant } from '../../hooks';
+import Text from '../../text';
 
 export default styled(Card)`
 	background: white;
 	padding: 5px;
+	height: 305px;
 `;
 
 const ImgContainer = styled('div')`
@@ -27,14 +31,77 @@ const ImgContainer = styled('div')`
 	}
 `;
 
-function Card({ resource, ...other }) {
+const Title = styled(Text).attrs({ as: 'h2' })`
+	font: normal 700 1rem/1.3 var(--header-font-family);
+	display: block;
+	color: var(--primary-grey);
+	text-transform: uppercase;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	margin: 0;
+
+	@media (--respond-to-handhelds) {
+		margin-bottom: 5px;
+		white-space: normal;
+	}
+
+	&.variant-list-item {
+		margin-bottom: 0.5rem;
+		margin-top: 0.25rem;
+	}
+`;
+
+const TokenList = ({ tokens = [], ...other }) =>
+	!tokens.length ? null : (
+		<ul {...other}>
+			{tokens.map(item => (
+				<li key={item}>
+					<Text.Label>{item}</Text.Label>
+				</li>
+			))}
+		</ul>
+	);
+
+const Meta = styled(TokenList)`
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	display: flex;
+	color: var(--tertiary-grey, #b8b8b8);
+
+	> * {
+		margin-right: 5px;
+		margin-top: 2px;
+	}
+`;
+
+const InfoContainer = styled('div')`
+	padding: 0 5px;
+`;
+
+const Byline = styled('div')`
+	font: normal 700 0.625rem/1.4 var(--body-font-family);
+	color: var(--primary-blue);
+	text-transform: uppercase;
+`;
+
+const metadata = ['SAMPL-1001', 'January 2021'];
+
+function Card({ resource, variant: v, className, ...other }) {
+	const variant = useListItemVariant(v);
+
 	return (
-		<StandardCard {...other}>
+		<StandardCard className={cx(variant, className)} {...other}>
 			<ImgContainer>
 				<Asset contentPackage={resource} type="landing">
 					<Image.Deferred alt="" />
 				</Asset>
 			</ImgContainer>
+			<InfoContainer>
+				<Meta tokens={metadata} />
+				<Title>This is the title</Title>
+				<Byline>test 1, test 2, test 3</Byline>
+			</InfoContainer>
 		</StandardCard>
 	);
 }
