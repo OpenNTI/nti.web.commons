c9d5f012e45ab84019b807bfe28036a375421636
Jonathan Grimes <jsg2021@gmail.com>
2021-03-18 17:27:49 -0500

    test vertical colum of tooltip

--- src/components/__stories__/Tooltip.stories.js
+++ src/components/__stories__/Tooltip.stories.js
@@ -18,3 +18,18 @@ export const MixedLabel = () => (
 		<p>Bring your cursor here</p>
 	</Tooltip>
 );
+
+export const VerticalColumn = () => (
+	<div
+		className={css`
+			display: flex;
+			flex-direction: column;
+		`}
+	>
+		{Array.from({ length: 10 }).map((_, i) => (
+			<Tooltip key={i} label={<button>This is weird!</button>}>
+				<p>Bring your cursor here</p>
+			</Tooltip>
+		))}
+	</div>
+);
