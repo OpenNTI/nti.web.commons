3ba1c88cfb1af56092779cc3f7b53ea84b025795
Jonathan Grimes <jsg2021@gmail.com>
2021-03-30 08:58:56 -0500

    formatting

--- test/app/items/ColorInput.jsx
+++ test/app/items/ColorInput.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import {Color} from '@nti/lib-commons';
 
-import {Input} from '../../../src';
+import { Color } from '@nti/lib-commons';
+
+import { Input } from '../../../src';
 
 const Presets = [
-	{color: Color.fromHex('#000000'), title: 'Black'},
-	{color: Color.fromHex('#ffffff'), title: 'White'},
-	{color: Color.fromHex('#d54e21'), title: 'Red'},
-	{color: Color.fromHex('#78a300'), title: 'Green'},
-	{color: Color.fromHex('#0e76a8'), title: 'Blue'},
-	{color: Color.fromHex('#9cc2cb'), title: 'Teal'}
+	{ color: Color.fromHex('#000000'), title: 'Black' },
+	{ color: Color.fromHex('#ffffff'), title: 'White' },
+	{ color: Color.fromHex('#d54e21'), title: 'Red' },
+	{ color: Color.fromHex('#78a300'), title: 'Green' },
+	{ color: Color.fromHex('#0e76a8'), title: 'Blue' },
+	{ color: Color.fromHex('#9cc2cb'), title: 'Teal' },
 ];
 
 export default class ColorInputTest extends React.Component {
 	//presets to be fetched from server at some point..
 	state = {
-		value: null
-	}
+		value: null,
+	};
 
-	onChange = (value) => {
-		console.log(value.hex.toString());//eslint-disable-line
-		this.setState({value});
-	}
+	onChange = value => {
+		// console.log(value.hex.toString());
+		this.setState({ value });
+	};
 
-	render () {
-		const {value} = this.state;
+	render() {
+		const { value } = this.state;
 
 		return (
 			<div>
@@ -36,10 +37,17 @@ export default class ColorInputTest extends React.Component {
 					verticalAlign={Input.Color.Flyout.ALIGNMENTS.BOTTOM}
 					horizontalAlign={Input.Color.Flyout.ALIGNMENTS.LEFT}
 				/>
-				<Input.Color.SaturationBrightness value={value} onChange={this.onChange} />
+				<Input.Color.SaturationBrightness
+					value={value}
+					onChange={this.onChange}
+				/>
 				<Input.Color.Hue value={value} onChange={this.onChange} />
 				<Input.Color.Text value={value} onChange={this.onChange} />
-				<Input.Color.PresetSwatches swatches={Presets} selected={value} onSelect={this.onChange}/>
+				<Input.Color.PresetSwatches
+					swatches={Presets}
+					selected={value}
+					onSelect={this.onChange}
+				/>
 			</div>
 		);
 	}
