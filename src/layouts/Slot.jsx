e9a913f31484bd01d647c41840850427d0ad5d6a
Ray Hatfield <ray.hatfield@gmail.com>
2021-04-08 16:42:33 -0500

    slot is a prop; make comparison smarter

--- src/layouts/Slot.jsx
+++ src/layouts/Slot.jsx
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 
 export const Slot = ({ slot, children }) => (
 	<>
-		{React.Children.toArray(children).filter(
-			({ props: { slot: s } }) => s === slot
+		{React.Children.toArray(children).filter(({ props: { slot: s } }) =>
+			!slot ? !s : s === slot
 		)}
 	</>
 );
 
 Slot.exists = (slot, children) =>
-	React.Children.toArray(children).some(c => c.slot === slot);
+	React.Children.toArray(children).some(({ props: { slot: s } }) =>
+		!slot ? !s : s === slot
+	);
 
 Slot.propTypes = {
 	slot: PropTypes.string,
