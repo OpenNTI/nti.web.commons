a9cd9450dc52962485e550fb7b7dbf3a297b765f
Ray Hatfield <ray.hatfield@gmail.com>
2021-04-08 16:18:55 -0500

    introduce Slot component

---
+++ src/layouts/Slot.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Slot = ({ slot, children }) =>
+	React.Children.toArray(children).filter(
+		({ props: { slot: s } }) => s === slot
+	);
+
+Slot.exists = (slot, children) =>
+	React.Children.toArray(children).some(c => c.slot === slot);
+
+Slot.propTypes = {
+	slot: PropTypes.string,
+	children: PropTypes.any,
+};
