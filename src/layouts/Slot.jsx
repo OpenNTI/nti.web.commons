62838494e639257211476ef224bed810f7fcb47c
Ray Hatfield <ray.hatfield@gmail.com>
2021-04-09 13:31:47 -0500

    add explanatory/documentation comment for Slot component

--- src/layouts/Slot.jsx
+++ src/layouts/Slot.jsx
@@ -3,6 +3,23 @@ import PropTypes from 'prop-types';
 
 const matchFn = slot => ({ props: { slot: s } }) => (!slot ? !s : s === slot);
 
+/*
+ * This component provides a mechanism by which a parent component
+ * can render provided children into designated 'slots' in its markup
+ *
+ * function Parent (props) {
+ * 	<section>
+ * 		<header>
+ * 			<Slot slot="header" {...props} /> <== render children with a slot="header" prop here
+ * 		</header>
+ * 		<Slot {...props} /> <== render children without a slot prop here
+ * 	</section>
+ * }
+ *
+ * <Parent>
+ * 		<h1 slot="header">{pageTitle}</h1> <== gets rendered into Parent's <header> element
+ * </Parent>
+ */
 export const Slot = ({ slot, children }) => (
 	<>{React.Children.toArray(children).filter(matchFn(slot))}</>
 );
