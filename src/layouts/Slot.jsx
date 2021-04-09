fb40f7a7c4ae2f0e2ed124849ae59a7b372ea6cb
Ray Hatfield <ray.hatfield@gmail.com>
2021-04-09 08:20:33 -0500

    DRY - factor out shared slot matching fn

--- src/layouts/Slot.jsx
+++ src/layouts/Slot.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const matchFn = slot => ({ props: { slot: s } }) => (!slot ? !s : s === slot);
+
 export const Slot = ({ slot, children }) => (
-	<>
-		{React.Children.toArray(children).filter(({ props: { slot: s } }) =>
-			!slot ? !s : s === slot
-		)}
-	</>
+	<>{React.Children.toArray(children).filter(matchFn(slot))}</>
 );
 
 Slot.exists = (slot, children) =>
-	React.Children.toArray(children).some(({ props: { slot: s } }) =>
-		!slot ? !s : s === slot
-	);
+	React.Children.toArray(children).some(matchFn(slot));
 
 Slot.propTypes = {
 	slot: PropTypes.string,
