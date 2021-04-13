1eea35e6b223d93810391768722ede07aaf0d627
Jonathan Grimes <jsg2021@gmail.com>
2021-04-13 13:38:07 -0500

    make common avatar use web-app styling

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -129,9 +129,14 @@ export default class Avatar extends BaseEntity {
 						onError={this.setUnknown}
 					/>
 				) : initials ? (
-					<svg {...childProps} viewBox="0 0 32 32">
+					<svg viewBox="0 0 32 32" {...childProps}>
 						<rect width="100%" height="100%" />
-						<text textAnchor="middle" x="16px" y="21px">
+						<text
+							dominantBaseline="central"
+							textAnchor="middle"
+							x="50%"
+							y="50%"
+						>
 							{initials}
 						</text>
 					</svg>
