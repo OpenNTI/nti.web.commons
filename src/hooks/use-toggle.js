7d2e7cc5ddc1fb893a0ac3ebc6ba4a44d12d88d1
Jonathan Grimes <jsg2021@gmail.com>
2021-04-14 16:46:07 -0500

    the toggle function consumes events

--- src/hooks/use-toggle.js
+++ src/hooks/use-toggle.js
@@ -2,6 +2,11 @@ import { useState, useCallback } from 'react';
 
 export function useToggle(initial = false) {
 	const [state, setState] = useState(initial);
-	const toggle = useCallback(() => setState(x => !x), []);
+	const toggle = useCallback(e => {
+		// if the toggle function is passed directly to an onClick, mark it handled.
+		e?.stopPropagation?.();
+		e?.preventDefault?.();
+		setState(x => !x), [];
+	});
 	return [state, toggle];
 }
