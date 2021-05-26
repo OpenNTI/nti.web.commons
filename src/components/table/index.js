7ed2742db159d6905f99db12bea4a9188fd4c45f
Jonathan Grimes <jsg2021@gmail.com>
2021-05-26 13:26:33 -0500

    make table exports more palatable to use

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,5 +1,19 @@
-export { default as ListTable } from './list-table/ListTable'; //deprecated
-export { default as Table } from './Table';
-export { default as TableHeader } from './TableHeader';
-export { default as TableRow } from './TableRow';
+import List from './list-table/ListTable'; //deprecated
+import Panel from './Table';
+import Header from './TableHeader';
+import Row from './TableRow';
+
+export {
+	//Backwards compat
+	List as ListTable,
+	Panel as Table,
+	Header as TableHeader,
+	Row as TableRow,
+	// Cleaner names:
+	List,
+	Panel,
+	Header,
+	Row,
+};
+
 export * from './Constants';
