9e3dab46c819c7dff24ab591abf73d0be4ee6adf
Jonathan Grimes <jsg2021@gmail.com>
2021-06-09 13:00:22 -0500

    proper deprecation mark

--- src/components/loading-indicators/index.js
+++ src/components/loading-indicators/index.js
@@ -1,7 +1,10 @@
-export { default as Ellipse } from './Ellipsis'; // deprecated
 export { default as Ellipsis } from './Ellipsis';
 export { default as Mask } from './Mask';
 export { default as Overlay } from './Overlay';
 export { default as Placeholder } from './Placeholder';
 export { default as Spinner } from './Spinner';
 export { default as Whacky } from './Whacky';
+
+import Ellipsis from './Ellipsis';
+/** @deprecated */
+export const Ellipse = Ellipsis;
