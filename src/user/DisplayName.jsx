961d422fb1929bb172f2dd7986350595c89676c5
Ray Hatfield <ray.hatfield@gmail.com>
2021-08-05 10:52:57 -0500

    typo

--- src/user/DisplayName.jsx
+++ src/user/DisplayName.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { DisplayName } from '../components';
 
 /**
- * Convenance helpers to reduce imports on commons if you just need User.x stuff
+ * Convenience helpers to reduce imports on commons if you just need User.x stuff
  *
  * @param {*} props
  * @returns {React.ReactElement}
