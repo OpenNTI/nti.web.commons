a31978fc6e0763e75211d48fda4a08c283612ae0
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-08-17 11:00:06 -0500

    Filter props

--- src/tabs/Tab.jsx
+++ src/tabs/Tab.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 
+import { filterProps } from '../utils';
+
 import styles from './Tab.css';
 
 const cx = classnames.bind(styles);
 
 export default function Tab({ label, active, className, ...props }) {
 	return (
-		<div className={cx('nti-tab', { active }, className)} {...props}>
+		<div
+			className={cx('nti-tab', { active }, className)}
+			{...filterProps(props, 'div')}
+		>
 			{label}
 		</div>
 	);
