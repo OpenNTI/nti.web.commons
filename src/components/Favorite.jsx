f6a9a2374db441268dfb63cf206f8539607268fc
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-10-12 13:08:49 -0500

    Favorite

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,9 +1,13 @@
-import './Favorite.scss';
+import React from 'react';
 import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
+import { Button } from '@nti/web-core';
+
 import { ItemChanges } from '../mixins';
+
+import './Favorite.scss';
 
 export default createReactClass({
 	displayName: 'Favorite',
@@ -32,10 +36,8 @@ export default createReactClass({
 			'button-like': asButton,
 		});
 
-		const Tag = asButton ? 'span' : 'a';
-		const extraProps = asButton
-			? { role: 'button', tabIndex: '0' }
-			: { href: '#' };
+		const Tag = asButton ? Button : 'a';
+		const extraProps = asButton ? { plain: true } : { href: '#' };
 
 		return <Tag {...extraProps} className={cls} onClick={this.onClick} />;
 	},
