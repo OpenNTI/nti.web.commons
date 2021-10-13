337d4e0b7dfeb5f1ba2cec8671db01fc016854f8
Jonathan Grimes <jsg2021@gmail.com>
2021-10-13 15:10:44 -0500

    lint

--- src/__mocks__/react-dom.disabled.js
+++ src/__mocks__/react-dom.disabled.js
@@ -1,7 +1,6 @@
 /* eslint-env jest */
 /* eslint no-console: 0, import/no-commonjs: 0 */
 
-const React = require('react');
 const reactDom = jest.genMockFromModule('react-dom');
 
 function mockCreatePortal(element, target) {
